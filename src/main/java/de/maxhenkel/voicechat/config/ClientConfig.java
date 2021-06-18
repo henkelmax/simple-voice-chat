b533b10516d002526b84ea17a4cc2e5590ae1c8e
Max Henkel <max@maxhenkel.de>
2021-06-18 16:09:27 +0200

    Add group HUD scale config option

--- src/main/java/de/maxhenkel/voicechat/config/ClientConfig.java
+++ src/main/java/de/maxhenkel/voicechat/config/ClientConfig.java
@@ -19,6 +19,7 @@ public class ClientConfig {
     public final ConfigBuilder.ConfigEntry<Boolean> stereo;
     public final ConfigBuilder.ConfigEntry<Boolean> hideIcons;
     public final ConfigBuilder.ConfigEntry<Boolean> showGroupHUD;
+    public final ConfigBuilder.ConfigEntry<Double> groupHudIconScale;
     public final ConfigBuilder.ConfigEntry<HUDIconLocation> hudIconLocation;
     public final ConfigBuilder.ConfigEntry<GroupPlayerIconOrientation> groupPlayerIconOrientation;
 
@@ -36,6 +37,7 @@ public class ClientConfig {
         stereo = builder.booleanEntry("stereo", true);
         hideIcons = builder.booleanEntry("hide_icons", false);
         showGroupHUD = builder.booleanEntry("show_group_hud", true);
+        groupHudIconScale = builder.doubleEntry("group_hud_icon_scale", 2D, 0.01D, 10D);
         hudIconLocation = builder.enumEntry("hud_icon_location", HUDIconLocation.LEFT);
         groupPlayerIconOrientation = builder.enumEntry("group_player_icon_orientation", GroupPlayerIconOrientation.VERTICAL);
     }
