252cacb382c9f810fbb316b43203b2f93cc00e09
Bread <36360408+mjwells2002@users.noreply.github.com>
2021-06-28 21:49:16 +0100

    Adjust LoginPluginAPI to work with 1.16 bukkit

--- src/main/java/de/maxhenkel/voicechat/util/LoginPluginAPI.java
+++ src/main/java/de/maxhenkel/voicechat/util/LoginPluginAPI.java
@@ -3,6 +3,7 @@ package de.maxhenkel.voicechat.util;
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
+import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import net.minecraft.server.v1_16_R3.MinecraftKey;
 import net.minecraft.server.v1_16_R3.PacketDataSerializer;
@@ -10,6 +11,7 @@ import net.minecraft.server.v1_16_R3.PacketLoginInCustomPayload;
 import net.minecraft.server.v1_16_R3.PacketLoginOutCustomPayload;
 
 import javax.annotation.Nullable;
+import java.io.IOException;
 
 public class LoginPluginAPI {
     private static final int PACKET_ID = 8712712;
@@ -18,9 +20,12 @@ public class LoginPluginAPI {
     public static PacketContainer generatePluginRequest(com.comphenix.protocol.wrappers.MinecraftKey channel, byte[] data) {
         MinecraftKey channelID = new MinecraftKey(channel.getPrefix(), channel.getKey());
         PacketDataSerializer tempBuffer = new PacketDataSerializer(Unpooled.buffer());
+        tempBuffer.d(PACKET_ID);
+        tempBuffer.a(channelID);
         tempBuffer.writeBytes(data);
         try {
-            PacketLoginOutCustomPayload rawPacketHandle = new PacketLoginOutCustomPayload(PACKET_ID, channelID, tempBuffer);
+            PacketLoginOutCustomPayload rawPacketHandle = new PacketLoginOutCustomPayload();
+            rawPacketHandle.a(tempBuffer);
 
             return new PacketContainer(PacketType.Login.Server.CUSTOM_PAYLOAD, rawPacketHandle);
         } catch (Exception e) {
@@ -32,17 +37,26 @@ public class LoginPluginAPI {
     @Nullable
     public static FriendlyByteBuf readPluginResponse(PacketEvent event) {
         PacketLoginInCustomPayload pluginResponsePacket = (PacketLoginInCustomPayload) event.getPacket().getHandle();
-        int id = pluginResponsePacket.getId();
-        PacketDataSerializer buf = pluginResponsePacket.getBuf();
-        if (buf == null && id == PACKET_ID) {
+        PacketDataSerializer pluginResponseData = new PacketDataSerializer(Unpooled.buffer());
+        try {
+            pluginResponsePacket.b(pluginResponseData);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        int id = pluginResponseData.i();
+        Boolean understood = pluginResponseData.readBoolean();
+        ByteBuf buf = pluginResponseData.readBytes(pluginResponseData.readableBytes());
+
+        if (!understood && id == PACKET_ID) {
             event.setCancelled(true);
             return null;
         }
         if (id == PACKET_ID) {
             event.setCancelled(true);
-            return new FriendlyByteBuf(pluginResponsePacket.getBuf());
+            return new FriendlyByteBuf(buf);
         }
         return null;
     }
-
 }
