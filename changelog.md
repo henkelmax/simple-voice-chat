9fbed17ed252cd4337a99171463b12463c8fc5a6
Max Henkel <max@maxhenkel.de>
2021-07-09 18:38:47 +0200

    Reduce errors when failing to read packets

--- changelog.md
+++ changelog.md
@@ -1,1 +1,1 @@
-- Updated to 1.17.1
+- Reduced errors when failing to read packets
--- src/main/java/de/maxhenkel/voicechat/voice/server/Server.java
+++ src/main/java/de/maxhenkel/voicechat/voice/server/Server.java
@@ -6,10 +6,14 @@ import de.maxhenkel.voicechat.voice.common.*;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import java.net.BindException;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.security.InvalidKeyException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -135,7 +139,15 @@ public class Server extends Thread {
                         continue;
                     }
 
-                    NetworkMessage message = NetworkMessage.readPacketServer(msg, Server.this);
+                    NetworkMessage message;
+                    try {
+                        message = NetworkMessage.readPacketServer(msg, Server.this);
+                    } catch (IndexOutOfBoundsException | BadPaddingException | NoSuchPaddingException | IllegalBlockSizeException | InvalidKeyException e) {
+                        CooldownTimer.run("failed_reading_packet", () -> {
+                            Voicechat.LOGGER.warn("Failed to read packet from {}", msg.getPacket().getSocketAddress());
+                        });
+                        continue;
+                    }
 
                     if (System.currentTimeMillis() - message.getTimestamp() > message.getTTL()) {
                         CooldownTimer.run("ttl", () -> {
