3c38cddd693a34f869ea5a4de5461675ab3b252b
Max Henkel <max@maxhenkel.de>
2021-09-11 13:06:17 +0200

    Port to 1.16.5

--- src/main/java/de/maxhenkel/voicechat/gui/widgets/BooleanConfigButton.java
+++ src/main/java/de/maxhenkel/voicechat/gui/widgets/BooleanConfigButton.java
@@ -1,9 +1,8 @@
 package de.maxhenkel.voicechat.gui.widgets;
 
-import net.minecraft.client.gui.components.AbstractButton;
-import net.minecraft.client.gui.narration.NarrationElementOutput;
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.TextComponent;
+import net.minecraft.client.gui.widget.button.AbstractButton;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraftforge.common.ForgeConfigSpec;
 
 import java.util.function.Function;
@@ -11,10 +10,10 @@ import java.util.function.Function;
 public class BooleanConfigButton extends AbstractButton {
 
     private ForgeConfigSpec.BooleanValue entry;
-    private Function<Boolean, Component> component;
+    private Function<Boolean, ITextComponent> component;
 
-    public BooleanConfigButton(int x, int y, int width, int height, ForgeConfigSpec.BooleanValue entry, Function<Boolean, Component> component) {
-        super(x, y, width, height, TextComponent.EMPTY);
+    public BooleanConfigButton(int x, int y, int width, int height, ForgeConfigSpec.BooleanValue entry, Function<Boolean, ITextComponent> component) {
+        super(x, y, width, height, StringTextComponent.EMPTY);
         this.entry = entry;
         this.component = component;
         updateText();
@@ -29,10 +28,5 @@ public class BooleanConfigButton extends AbstractButton {
         entry.set(!entry.get());
         entry.save();
         updateText();
-    }
-
-    @Override
-    public void updateNarration(NarrationElementOutput narrationElementOutput) {
-        defaultButtonNarrationText(narrationElementOutput);
     }
 }
