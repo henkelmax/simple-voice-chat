e9984ca3a8e203727c1f50adc78b9c8351fc837e
Max Henkel <max@maxhenkel.de>
2021-09-12 10:04:11 +0200

    Update dependencies

--- src/main/java/de/maxhenkel/voicechat/config/ServerConfig.java
+++ src/main/java/de/maxhenkel/voicechat/config/ServerConfig.java
@@ -1,5 +1,7 @@
 package de.maxhenkel.voicechat.config;
 
+import de.maxhenkel.configbuilder.ConfigBuilder;
+
 public class ServerConfig {
 
     public final ConfigBuilder.ConfigEntry<Integer> voiceChatPort;
