ba4ecee779477ea87d5f9a3ab9d9753f24b40a84
Max Henkel <max@maxhenkel.de>
2021-10-17 12:31:47 +0200

    Add publish to LAN message on fabric

---
+++ fabric/src/main/java/de/maxhenkel/voicechat/mixin/IntegratedServerMixin.java
@@ -0,0 +1,30 @@
+package de.maxhenkel.voicechat.mixin;
+
+import de.maxhenkel.voicechat.Voicechat;
+import de.maxhenkel.voicechat.voice.server.Server;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.server.IntegratedServer;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.level.GameType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import javax.annotation.Nullable;
+
+@Mixin(IntegratedServer.class)
+public class IntegratedServerMixin {
+
+    @Inject(method = "publishServer", at = @At(value = "RETURN"))
+    public void publishServer(@Nullable GameType gameType, boolean cheats, int port, CallbackInfoReturnable<Boolean> callbackInfo) {
+        if (!callbackInfo.getReturnValue()) {
+            return;
+        }
+        Server server = Voicechat.SERVER.getServer();
+        if (server != null) {
+            Minecraft.getInstance().gui.getChat().addMessage(new TranslatableComponent("message.voicechat.server_port", server.getPort()));
+        }
+    }
+
+}
