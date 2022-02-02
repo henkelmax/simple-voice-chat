0f854680fdcb75f0dc276dc69fe9cd7e73a5fd77
Max Henkel <max@maxhenkel.de>
2022-02-02 18:00:03 +0100

    Add client audio data override to API

--- common/src/main/java/de/maxhenkel/voicechat/plugins/impl/events/ClientSoundEventImpl.java
+++ common/src/main/java/de/maxhenkel/voicechat/plugins/impl/events/ClientSoundEventImpl.java
@@ -5,15 +5,27 @@ import de.maxhenkel.voicechat.api.events.ClientSoundEvent;
 
 public class ClientSoundEventImpl extends ClientEventImpl implements ClientSoundEvent {
 
-    private final short[] rawAudio;
+    private short[] rawAudio;
+    private boolean whispering;
 
-    public ClientSoundEventImpl(VoicechatClientApi api, short[] rawAudio) {
+    public ClientSoundEventImpl(VoicechatClientApi api, short[] rawAudio, boolean whispering) {
         super(api);
         this.rawAudio = rawAudio;
+        this.whispering = whispering;
     }
 
     @Override
     public short[] getRawAudio() {
         return rawAudio;
+    }
+
+    @Override
+    public void setRawAudio(short[] rawAudio) {
+        this.rawAudio = rawAudio;
+    }
+
+    @Override
+    public boolean isWhispering() {
+        return whispering;
     }
 }
