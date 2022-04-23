e6f14e2056f05734a9dfab1c26f911713ec18cc1
Max Henkel <max@maxhenkel.de>
2022-04-23 11:34:40 +0200

    Use service loader to load platform specific classes

--- forge/src/main/java/de/maxhenkel/voicechat/ForgeVoicechatClientMod.java
+++ forge/src/main/java/de/maxhenkel/voicechat/ForgeVoicechatClientMod.java
@@ -4,7 +4,6 @@ import com.sun.jna.Platform;
 import de.maxhenkel.voicechat.config.ForgeClientConfig;
 import de.maxhenkel.voicechat.gui.VoiceChatSettingsScreen;
 import de.maxhenkel.voicechat.intercompatibility.ClientCompatibilityManager;
-import de.maxhenkel.voicechat.intercompatibility.ForgeClientCompatibilityManager;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fmlclient.ConfigGuiHandler;
@@ -14,17 +13,14 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 public class ForgeVoicechatClientMod extends VoicechatClient {
 
-    private final ClientCompatibilityManager compatibilityManager;
-
     public ForgeVoicechatClientMod() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
-        compatibilityManager = new ForgeClientCompatibilityManager();
         CLIENT_CONFIG = ForgeVoicechatMod.registerConfig(ModConfig.Type.CLIENT, ForgeClientConfig::new);
     }
 
     public void clientSetup(FMLClientSetupEvent event) {
         initializeClient();
-        MinecraftForge.EVENT_BUS.register(compatibilityManager);
+        MinecraftForge.EVENT_BUS.register(ClientCompatibilityManager.INSTANCE);
         ModLoadingContext.get().registerExtensionPoint(ConfigGuiHandler.ConfigGuiFactory.class, () -> new ConfigGuiHandler.ConfigGuiFactory((client, parent) -> {
             return new VoiceChatSettingsScreen(parent);
         }));
@@ -32,10 +28,5 @@ public class ForgeVoicechatClientMod extends VoicechatClient {
         if (Platform.isMac() && !CLIENT_CONFIG.javaMicrophoneImplementation.get()) {
             CLIENT_CONFIG.javaMicrophoneImplementation.set(true).save();
         }
-    }
-
-    @Override
-    public ClientCompatibilityManager createCompatibilityManager() {
-        return compatibilityManager;
     }
 }
