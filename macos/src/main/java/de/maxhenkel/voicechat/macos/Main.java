fed95ed62077f6b944b0a18bd4111717e65e7529
Max Henkel <max@maxhenkel.de>
2022-05-23 10:44:16 +0200

    Remove preprocessor in MacOS patcher

--- macos/src/main/java/de/maxhenkel/voicechat/macos/Main.java
+++ macos/src/main/java/de/maxhenkel/voicechat/macos/Main.java
@@ -4,10 +4,24 @@ import com.sun.jna.Platform;
 import de.maxhenkel.voicechat.macos.jna.avfoundation.AVAuthorizationStatus;
 
 import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.jar.Manifest;
 
 public class Main {
 
-    public static final String VERSION = "@PATCHER_VERSION@";
+    public static final String VERSION;
+
+    static {
+        String version = "N/A";
+        try {
+            version = readVersion();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        VERSION = version;
+    }
 
     public static void main(String[] args) {
         System.out.printf("MacOS patcher version %s%n", VERSION);
@@ -65,6 +79,23 @@ public class Main {
         }
 
         new MacosFrame();
+    }
+
+    private static String readVersion() throws IOException {
+        Enumeration<URL> resources = Main.class.getClassLoader().getResources("META-INF/MANIFEST.MF");
+        while (resources.hasMoreElements()) {
+            Manifest manifest = new Manifest(resources.nextElement().openStream());
+            String version = manifest.getMainAttributes().getValue("Patcher-Version");
+            if (version != null) {
+                return version;
+            }
+        }
+        // Use the environment variable in development
+        String env = System.getenv("PATCHER_VERSION");
+        if (env != null) {
+            return env;
+        }
+        throw new IOException("Could not read MANIFEST.MF");
     }
 
 }
