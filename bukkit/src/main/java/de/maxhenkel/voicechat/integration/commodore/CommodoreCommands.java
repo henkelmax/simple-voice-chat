a195601c2f13d5d4e078f2088070869c51997660
Max Henkel <max@maxhenkel.de>
2022-06-13 08:28:14 +0200

    Fix bukkit command completion

--- bukkit/src/main/java/de/maxhenkel/voicechat/integration/commodore/CommodoreCommands.java
+++ bukkit/src/main/java/de/maxhenkel/voicechat/integration/commodore/CommodoreCommands.java
@@ -1,23 +1,35 @@
 package de.maxhenkel.voicechat.integration.commodore;
 
+import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import de.maxhenkel.voicechat.command.VoiceChatCommands;
 import me.lucko.commodore.Commodore;
+import net.minecraft.commands.arguments.ArgumentEntity;
+import net.minecraft.commands.arguments.ArgumentUUID;
 
 public class CommodoreCommands {
 
     public static void registerCompletions(Commodore commodore) {
         LiteralArgumentBuilder<?> literalBuilder = LiteralArgumentBuilder.literal(VoiceChatCommands.VOICECHAT_COMMAND)
                 .then(LiteralArgumentBuilder.literal("help"))
-                .then(LiteralArgumentBuilder.literal("test").then(RequiredArgumentBuilder.argument("target", EntityArgument.entity(true, true))))
-                .then(LiteralArgumentBuilder.literal("invite").then(RequiredArgumentBuilder.argument("target", EntityArgument.entity(true, true))))
-                .then(LiteralArgumentBuilder.literal("join").then(RequiredArgumentBuilder.argument("group", UUIDArgument.uuid())))
-                .then(LiteralArgumentBuilder.literal("join").then(RequiredArgumentBuilder.argument("group", UUIDArgument.uuid()).then(RequiredArgumentBuilder.argument("password", StringArgumentType.string()))))
+                .then(LiteralArgumentBuilder.literal("test").then(RequiredArgumentBuilder.argument("target", playerArgument())))
+                .then(LiteralArgumentBuilder.literal("invite").then(RequiredArgumentBuilder.argument("target", playerArgument())))
+                .then(LiteralArgumentBuilder.literal("join").then(RequiredArgumentBuilder.argument("group", uuidArgument())))
+                .then(LiteralArgumentBuilder.literal("join").then(RequiredArgumentBuilder.argument("group", uuidArgument()).then(RequiredArgumentBuilder.argument("password", StringArgumentType.string()))))
                 .then(LiteralArgumentBuilder.literal("leave"));
 
         commodore.register(literalBuilder);
+    }
+
+    private static ArgumentType<?> playerArgument() {
+        // return new ArgumentEntity(true, true);
+        return ArgumentEntity.c();
+    }
+
+    private static ArgumentType<?> uuidArgument() {
+        return ArgumentUUID.a();
     }
 
 }
