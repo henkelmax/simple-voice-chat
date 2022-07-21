fdd4361b9c99b09ae97a880c457cf8c63eaaeb8d
Max Henkel <max@maxhenkel.de>
2022-07-21 14:32:21 +0200

    Backport changes to 1.18.2

--- common/src/main/java/de/maxhenkel/voicechat/gui/VoiceChatScreen.java
+++ common/src/main/java/de/maxhenkel/voicechat/gui/VoiceChatScreen.java
@@ -19,6 +19,7 @@ import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
