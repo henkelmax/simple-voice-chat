3fb2812748093c9c47753d1b0e87a4702f4d8227
Max Henkel <max@maxhenkel.de>
2022-07-21 14:09:44 +0200

    Remove voice chat debug report

--- quilt/src/main/java/de/maxhenkel/voicechat/intercompatibility/QuiltCommonCompatibilityManager.java
+++ quilt/src/main/java/de/maxhenkel/voicechat/intercompatibility/QuiltCommonCompatibilityManager.java
@@ -1,6 +1,5 @@
 package de.maxhenkel.voicechat.intercompatibility;
 
-import com.mojang.blaze3d.platform.InputConstants;
 import com.mojang.brigadier.CommandDispatcher;
 import de.maxhenkel.voicechat.Voicechat;
 import de.maxhenkel.voicechat.api.VoicechatPlugin;
@@ -11,21 +10,16 @@ import de.maxhenkel.voicechat.net.QuiltNetManager;
 import de.maxhenkel.voicechat.permission.PermissionManager;
 import de.maxhenkel.voicechat.permission.QuiltPermissionManager;
 import net.fabricmc.api.EnvType;
-import net.fabricmc.fabric.impl.client.keybinding.KeyBindingRegistryImpl;
-import net.fabricmc.fabric.mixin.client.keybinding.KeyCodeAccessor;
-import net.minecraft.client.KeyMapping;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
 import org.quiltmc.loader.api.ModContainer;
-import org.quiltmc.loader.api.ModMetadata;
 import org.quiltmc.loader.api.QuiltLoader;
 import org.quiltmc.loader.api.entrypoint.EntrypointContainer;
 import org.quiltmc.loader.api.minecraft.MinecraftQuiltLoader;
 import org.quiltmc.qsl.command.api.CommandRegistrationCallback;
 import org.quiltmc.qsl.lifecycle.api.event.ServerLifecycleEvents;
 
-import java.lang.reflect.Field;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.UUID;
@@ -120,41 +114,6 @@ public class QuiltCommonCompatibilityManager extends CommonCompatibilityManager 
             netManager = new QuiltNetManager();
         }
         return netManager;
-    }
-
-    @Override
-    public String listLoadedMods() {
-        StringBuilder sb = new StringBuilder();
-        for (ModContainer mod : QuiltLoader.getAllMods()) {
-            ModMetadata metadata = mod.metadata();
-            sb.append("Mod ID: " + metadata.id());
-            sb.append("\n");
-            sb.append("Name: " + metadata.name());
-            sb.append("\n");
-            sb.append("Version: " + metadata.version().raw());
-            sb.append("\n\n");
-        }
-        return sb.toString();
-    }
-
-    @Override
-    public String listKeybinds() {
-        StringBuilder sb = new StringBuilder();
-        try {
-            Field moddedKeyBindings = KeyBindingRegistryImpl.class.getDeclaredField("moddedKeyBindings");
-            moddedKeyBindings.setAccessible(true);
-            List<KeyMapping> mappings = (List<KeyMapping>) moddedKeyBindings.get(null);
-            for (KeyMapping mapping : mappings) {
-                InputConstants.Key boundKey = ((KeyCodeAccessor) mapping).fabric_getBoundKey();
-                sb.append(mapping.getName() + "(" + mapping.getCategory() + "): " + boundKey.getName() + " (" + mapping.getDefaultKey().getName() + ")");
-                sb.append("\n");
-            }
-            sb.append("\n");
-        } catch (Exception e) {
-            sb.append("Error: " + e.getMessage());
-            sb.append("\n");
-        }
-        return sb.toString();
     }
 
     @Override
