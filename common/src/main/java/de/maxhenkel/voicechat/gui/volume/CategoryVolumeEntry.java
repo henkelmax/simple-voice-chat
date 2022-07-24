9b3b8e1b13e676bc9f7cc1459fa750b0c339292c
Max Henkel <max@maxhenkel.de>
2022-07-24 12:16:39 +0200

    Fix custom category icons not displaying correctly

--- common/src/main/java/de/maxhenkel/voicechat/gui/volume/CategoryVolumeEntry.java
+++ common/src/main/java/de/maxhenkel/voicechat/gui/volume/CategoryVolumeEntry.java
@@ -3,17 +3,20 @@ package de.maxhenkel.voicechat.gui.volume;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import de.maxhenkel.voicechat.VoicechatClient;
 import de.maxhenkel.voicechat.plugins.impl.VolumeCategoryImpl;
-import de.maxhenkel.voicechat.voice.client.ClientCategoryManager;
+import de.maxhenkel.voicechat.voice.client.ClientManager;
 import net.minecraft.client.gui.AbstractGui;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.StringTextComponent;
 
 public class CategoryVolumeEntry extends VolumeEntry {
 
     protected final VolumeCategoryImpl category;
+    protected final ResourceLocation texture;
 
     public CategoryVolumeEntry(VolumeCategoryImpl category, AdjustVolumesScreen screen) {
         super(screen, new CategoryVolumeConfigEntry(category.getId()));
         this.category = category;
+        this.texture = ClientManager.getCategoryManager().getTexture(category.getId(), OTHER_VOLUME_ICON);
     }
 
     public VolumeCategoryImpl getCategory() {
@@ -22,11 +25,7 @@ public class CategoryVolumeEntry extends VolumeEntry {
 
     @Override
     public void renderElement(MatrixStack poseStack, int index, int top, int left, int width, int height, int mouseX, int mouseY, boolean hovered, float delta, int skinX, int skinY, int textX, int textY) {
-        if (category.getIcon() == null) {
-            minecraft.getTextureManager().bind(OTHER_VOLUME_ICON);
-        } else {
-            minecraft.getTextureManager().bind(ClientCategoryManager.getTexture(category.getId()));
-        }
+        minecraft.getTextureManager().bind(texture);
         AbstractGui.blit(poseStack, skinX, skinY, SKIN_SIZE, SKIN_SIZE, 16, 16, 16, 16, 16, 16);
         minecraft.font.draw(poseStack, new StringTextComponent(category.getName()), (float) textX, (float) textY, PLAYER_NAME_COLOR);
         if (hovered && category.getDescription() != null) {
