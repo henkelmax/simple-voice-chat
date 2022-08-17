e20ecea5f78dbacd6f2c37853beb46264d998bd4
Max Henkel <max@maxhenkel.de>
2022-08-17 09:16:46 +0200

    Rename native initialization thread

--- common/src/main/java/de/maxhenkel/voicechat/voice/common/Utils.java
+++ common/src/main/java/de/maxhenkel/voicechat/voice/common/Utils.java
@@ -216,7 +216,7 @@ public class Utils {
                 });
             }
             obj.set(supplier.get());
-        });
+        }, "NativeInitializationThread");
         t.start();
 
         try {
