4296f90e2a1686c1bcfbe44566f6bceb302c81ad
senseiwells <66843746+senseiwells@users.noreply.github.com>
2022-08-25 20:26:09 +0100

    Load server config before server starts (#457)

--- fabric/src/main/java/de/maxhenkel/voicechat/FabricVoicechatMod.java
+++ fabric/src/main/java/de/maxhenkel/voicechat/FabricVoicechatMod.java
@@ -3,15 +3,13 @@ package de.maxhenkel.voicechat;
 import de.maxhenkel.configbuilder.ConfigBuilder;
 import de.maxhenkel.voicechat.config.FabricServerConfig;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.loader.api.FabricLoader;
 
 public class FabricVoicechatMod extends Voicechat implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        ServerLifecycleEvents.SERVER_STARTED.register(server -> {
-            SERVER_CONFIG = ConfigBuilder.build(server.getServerDirectory().toPath().resolve("config").resolve(MODID).resolve("voicechat-server.properties"), true, FabricServerConfig::new);
-        });
+        SERVER_CONFIG = ConfigBuilder.build(FabricLoader.getInstance().getConfigDir().resolve(MODID).resolve("voicechat-server.properties"), true, FabricServerConfig::new);
 
         initialize();
     }
