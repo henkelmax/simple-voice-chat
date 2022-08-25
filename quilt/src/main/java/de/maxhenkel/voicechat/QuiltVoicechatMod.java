a089fb437d10a5acbe6df96fb48cc7a940e54f8a
Sensei <wellsm488@gmail.com>
2022-08-25 17:47:51 +0100

    Load server config before server starts

--- quilt/src/main/java/de/maxhenkel/voicechat/QuiltVoicechatMod.java
+++ quilt/src/main/java/de/maxhenkel/voicechat/QuiltVoicechatMod.java
@@ -3,16 +3,14 @@ package de.maxhenkel.voicechat;
 import de.maxhenkel.configbuilder.ConfigBuilder;
 import de.maxhenkel.voicechat.config.QuiltServerConfig;
 import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.loader.api.QuiltLoader;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
-import org.quiltmc.qsl.lifecycle.api.event.ServerLifecycleEvents;
 
 public class QuiltVoicechatMod extends Voicechat implements ModInitializer {
 
     @Override
     public void onInitialize(ModContainer mod) {
-        ServerLifecycleEvents.READY.register(server -> {
-            SERVER_CONFIG = ConfigBuilder.build(server.getServerDirectory().toPath().resolve("config").resolve(MODID).resolve("voicechat-server.properties"), true, QuiltServerConfig::new);
-        });
+        SERVER_CONFIG = ConfigBuilder.build(QuiltLoader.getConfigDir().resolve(MODID).resolve("voicechat-server.properties"), true, QuiltServerConfig::new);
 
         initialize();
     }
