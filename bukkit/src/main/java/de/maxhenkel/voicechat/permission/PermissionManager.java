e8acb3d106b5f955b4f8a8fa79bd0072c4d8f280
Max Henkel <max@maxhenkel.de>
2022-11-04 14:00:26 +0100

    Replace connect permission with listen permission

--- bukkit/src/main/java/de/maxhenkel/voicechat/permission/PermissionManager.java
+++ bukkit/src/main/java/de/maxhenkel/voicechat/permission/PermissionManager.java
@@ -1,13 +1,14 @@
 package de.maxhenkel.voicechat.permission;
 
+import de.maxhenkel.voicechat.Voicechat;
 import org.bukkit.permissions.Permission;
 import org.bukkit.permissions.PermissionDefault;
 
 public class PermissionManager {
 
-    public static Permission CONNECT_PERMISSION = new Permission("voicechat.connect", PermissionDefault.TRUE);
-    public static Permission SPEAK_PERMISSION = new Permission("voicechat.speak", PermissionDefault.TRUE);
-    public static Permission GROUPS_PERMISSION = new Permission("voicechat.groups", PermissionDefault.TRUE);
-    public static Permission ADMIN_PERMISSION = new Permission("voicechat.admin", PermissionDefault.OP);
+    public static Permission LISTEN_PERMISSION = new Permission(Voicechat.MODID + ".listen", PermissionDefault.TRUE);
+    public static Permission SPEAK_PERMISSION = new Permission(Voicechat.MODID + ".speak", PermissionDefault.TRUE);
+    public static Permission GROUPS_PERMISSION = new Permission(Voicechat.MODID + ".groups", PermissionDefault.TRUE);
+    public static Permission ADMIN_PERMISSION = new Permission(Voicechat.MODID + ".admin", PermissionDefault.OP);
 
 }
