146a543fd4bec8c653f3f55a5384f0f414838075
Max Henkel <max@maxhenkel.de>
2022-11-15 09:29:52 +0100

    Fix selected voice chat resource packs not persistent on Forge Closes #485

--- common/src/main/java/de/maxhenkel/voicechat/VoicechatClient.java
+++ common/src/main/java/de/maxhenkel/voicechat/VoicechatClient.java
@@ -32,14 +32,7 @@ public abstract class VoicechatClient {
     public static VoiceChatResourcePack WHITE_ICONS;
     public static VoiceChatResourcePack BLACK_ICONS;
 
-    public void initializeClient() {
-        fixVolumeConfig();
-        VOLUME_CONFIG = new PlayerVolumeConfig(Minecraft.getInstance().gameDirectory.toPath().resolve("config").resolve(Voicechat.MODID).resolve("voicechat-volumes.properties"));
-        USERNAME_CACHE = new UsernameCache(Minecraft.getInstance().gameDirectory.toPath().resolve("config").resolve(Voicechat.MODID).resolve("username-cache.json").toFile());
-
-        //Load instance
-        ClientManager.instance();
-
+    public VoicechatClient() {
         CLASSIC_ICONS = new VoiceChatResourcePack("classic_icons", new TranslatableComponent("resourcepack.voicechat.classic_icons"));
         WHITE_ICONS = new VoiceChatResourcePack("white_icons", new TranslatableComponent("resourcepack.voicechat.white_icons"));
         BLACK_ICONS = new VoiceChatResourcePack("black_icons", new TranslatableComponent("resourcepack.voicechat.black_icons"));
@@ -49,6 +42,15 @@ public abstract class VoicechatClient {
             consumer.accept(WHITE_ICONS.toPack());
             consumer.accept(BLACK_ICONS.toPack());
         });
+    }
+
+    public void initializeClient() {
+        fixVolumeConfig();
+        VOLUME_CONFIG = new PlayerVolumeConfig(Minecraft.getInstance().gameDirectory.toPath().resolve("config").resolve(Voicechat.MODID).resolve("voicechat-volumes.properties"));
+        USERNAME_CACHE = new UsernameCache(Minecraft.getInstance().gameDirectory.toPath().resolve("config").resolve(Voicechat.MODID).resolve("username-cache.json").toFile());
+
+        //Load instance
+        ClientManager.instance();
 
         if (VoicechatClient.CLIENT_CONFIG.macosMicrophoneWorkaround.get() && Platform.isMac()) {
             if (VersionCheck.isCompatible()) {
