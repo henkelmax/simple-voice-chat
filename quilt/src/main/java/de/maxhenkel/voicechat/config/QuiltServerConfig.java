adb06567bcbebba1e18627d1712104a75a7853bf
Max Henkel <max@maxhenkel.de>
2022-11-25 11:22:55 +0100

    Update config builder

--- quilt/src/main/java/de/maxhenkel/voicechat/config/QuiltServerConfig.java
+++ quilt/src/main/java/de/maxhenkel/voicechat/config/QuiltServerConfig.java
@@ -5,23 +5,7 @@ import de.maxhenkel.configbuilder.ConfigBuilder;
 public class QuiltServerConfig extends ServerConfig {
 
     public QuiltServerConfig(ConfigBuilder builder) {
-        voiceChatPort = builder.integerEntry("port", 24454, -1, 65535);
-        voiceChatBindAddress = builder.stringEntry("bind_address", "");
-        voiceChatDistance = builder.doubleEntry("max_voice_distance", 48D, 1D, 1_000_000D);
-        crouchDistanceMultiplier = builder.doubleEntry("crouch_distance_multiplier", 1D, 0.01D, 1D);
-        whisperDistanceMultiplier = builder.doubleEntry("whisper_distance_multiplier", 0.5D, 0.01D, 1D);
-        voiceChatCodec = builder.enumEntry("codec", Codec.VOIP);
-        voiceChatMtuSize = builder.integerEntry("mtu_size", 1024, 256, 10000);
-        keepAlive = builder.integerEntry("keep_alive", 1000, 1000, Integer.MAX_VALUE);
-        groupsEnabled = builder.booleanEntry("enable_groups", true);
-        openGroups = builder.booleanEntry("open_groups", false);
-        voiceHost = builder.stringEntry("voice_host", "");
-        allowRecording = builder.booleanEntry("allow_recording", true);
-        spectatorInteraction = builder.booleanEntry("spectator_interaction", false);
-        spectatorPlayerPossession = builder.booleanEntry("spectator_player_possession", false);
-        forceVoiceChat = builder.booleanEntry("force_voice_chat", false);
-        loginTimeout = builder.integerEntry("login_timeout", 10_000, 100, Integer.MAX_VALUE);
-        broadcastRange = builder.doubleEntry("broadcast_range", -1D, -1D, Double.MAX_VALUE);
+        super(builder);
     }
 
 }
