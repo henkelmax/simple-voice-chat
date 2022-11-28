f7826e3e3b6192170e69bb1da4510e9209288bf5
Max Henkel <max@maxhenkel.de>
2022-11-28 14:11:13 +0100

    Rework MacOS microphone permission check

---
+++ common/src/main/java/de/maxhenkel/voicechat/macos/PermissionCheck.java
@@ -0,0 +1,31 @@
+package de.maxhenkel.voicechat.macos;
+
+import de.maxhenkel.voicechat.macos.avfoundation.AVAuthorizationStatus;
+import de.maxhenkel.voicechat.macos.avfoundation.AVCaptureDevice;
+import de.maxhenkel.voicechat.macos.foundation.NSString;
+
+public class PermissionCheck {
+
+    private static final NSString AVMediaTypeAudio = new NSString("soun");
+
+    public static void requestMicrophonePermissions() {
+        checkMicrophonePermissions(true);
+    }
+
+    public static AVAuthorizationStatus getMicrophonePermissions() {
+        return checkMicrophonePermissions(false);
+    }
+
+    public static AVAuthorizationStatus checkMicrophonePermissions(boolean requestIfNeeded) {
+        if (!VersionCheck.isCompatible()) {
+            return AVAuthorizationStatus.AUTHORIZED;
+        }
+
+        var status = AVCaptureDevice.getAuthorizationStatus(AVMediaTypeAudio);
+        if (requestIfNeeded && status == AVAuthorizationStatus.NOT_DETERMINED) {
+            AVCaptureDevice.requestAccessForMediaType(AVMediaTypeAudio);
+        }
+
+        return status;
+    }
+}
