5705222ba00cf845996bbd9adf360426f1cffc9b
Max Henkel <max@maxhenkel.de>
2022-12-23 09:53:08 +0100

    Disable natives on unsupported MacOS versions

--- forge/src/main/java/de/maxhenkel/voicechat/ForgeVoicechatClientMod.java
+++ forge/src/main/java/de/maxhenkel/voicechat/ForgeVoicechatClientMod.java
@@ -5,6 +5,7 @@ import de.maxhenkel.voicechat.config.ForgeClientConfig;
 import de.maxhenkel.voicechat.gui.VoiceChatSettingsScreen;
 import de.maxhenkel.voicechat.intercompatibility.ClientCompatibilityManager;
 import net.minecraftforge.client.ConfigGuiHandler;
+import de.maxhenkel.voicechat.macos.VersionCheck;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.config.ModConfig;
@@ -27,6 +28,9 @@ public class ForgeVoicechatClientMod extends VoicechatClient {
 
         if (Platform.isMac() && !CLIENT_CONFIG.javaMicrophoneImplementation.get()) {
             CLIENT_CONFIG.javaMicrophoneImplementation.set(true).save();
+        }
+        if (Platform.isMac() && CLIENT_CONFIG.useNatives.get() && !VersionCheck.isMacOSNativeCompatible()) {
+            CLIENT_CONFIG.useNatives.set(false).save();
         }
     }
 }
