f3dac7a4d7fd9b88134e47e2fb2bb87c228d3355
Max Henkel <max@maxhenkel.de>
2023-02-27 18:33:46 +0100

    Improve group screen performance

--- common/src/main/java/de/maxhenkel/voicechat/gui/group/GroupScreen.java
+++ common/src/main/java/de/maxhenkel/voicechat/gui/group/GroupScreen.java
@@ -105,7 +105,6 @@ public class GroupScreen extends ListScreenBase {
     public void tick() {
         super.tick();
         checkButtons();
-        groupList.tick();
     }
 
     private void checkButtons() {
