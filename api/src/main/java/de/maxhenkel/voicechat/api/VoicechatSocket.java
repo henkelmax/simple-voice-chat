e3b96fd10694be88981da9269f2bd2c0e9fd7a67
Max Henkel <max@maxhenkel.de>
2023-02-27 17:43:26 +0100

    Update voice chat API javadocs

--- api/src/main/java/de/maxhenkel/voicechat/api/VoicechatSocket.java
+++ api/src/main/java/de/maxhenkel/voicechat/api/VoicechatSocket.java
@@ -2,6 +2,10 @@ package de.maxhenkel.voicechat.api;
 
 import java.net.SocketAddress;
 
+/**
+ * A socket used for server side voice chat traffic.
+ * Can be set using {@link de.maxhenkel.voicechat.api.events.VoicechatServerStartingEvent#setSocketImplementation(VoicechatSocket)}.
+ */
 public interface VoicechatSocket {
 
     void open(int port, String bindAddress) throws Exception;
