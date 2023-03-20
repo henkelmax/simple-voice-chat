d92565d98121e9344944f99563fa4fadcb1fc577
Max Henkel <max@maxhenkel.de>
2023-03-20 18:21:53 +0100

    Update rnnoise4j

--- common/src/main/java/de/maxhenkel/voicechat/voice/client/Denoiser.java
+++ common/src/main/java/de/maxhenkel/voicechat/voice/client/Denoiser.java
@@ -1,58 +1,23 @@
 package de.maxhenkel.voicechat.voice.client;
 
 import com.sun.jna.Platform;
-import com.sun.jna.Pointer;
-import de.maxhenkel.rnnoise4j.RNNoise;
+import de.maxhenkel.rnnoise4j.UnknownPlatformException;
 import de.maxhenkel.voicechat.Voicechat;
 import de.maxhenkel.voicechat.VoicechatClient;
 import de.maxhenkel.voicechat.macos.VersionCheck;
 import de.maxhenkel.voicechat.voice.common.Utils;
 
 import javax.annotation.Nullable;
+import java.io.IOException;
 
-public class Denoiser {
+public class Denoiser extends de.maxhenkel.rnnoise4j.Denoiser {
 
-    private static final int FRAME_SIZE = 480;
-
-    private final Pointer state;
-    private boolean closed;
-
-    private Denoiser() {
-        state = RNNoise.INSTANCE.rnnoise_create(null);
-    }
-
-    public short[] denoise(short[] audio) {
-        if (closed) {
-            throw new IllegalStateException("Tried to denoise with a closed denoiser");
-        }
-        float[] data = Utils.shortsToFloats(audio);
-        if (data.length % FRAME_SIZE != 0) {
-            throw new IllegalArgumentException("Denoising data frame size is not divisible by 480");
-        }
-        float[] chunk = new float[FRAME_SIZE];
-        float[] denoisedChunk = new float[FRAME_SIZE];
-        float[] denoised = new float[data.length];
-        for (int i = 0; i < data.length / FRAME_SIZE; i++) {
-            System.arraycopy(data, FRAME_SIZE * i, chunk, 0, FRAME_SIZE);
-            RNNoise.INSTANCE.rnnoise_process_frame(state, denoisedChunk, chunk);
-            System.arraycopy(denoisedChunk, 0, denoised, FRAME_SIZE * i, FRAME_SIZE);
-        }
-        return Utils.floatsToShorts(denoised);
-    }
-
-    public boolean isClosed() {
-        return closed;
-    }
-
-    public void close() {
-        if (closed) {
-            return;
-        }
-        closed = true;
-        RNNoise.INSTANCE.rnnoise_destroy(state);
+    private Denoiser() throws IOException, UnknownPlatformException {
+        super();
     }
 
     public static boolean supportsRNNoise() {
+        //TODO Fix check
         if (Platform.isMac()) {
             return VersionCheck.isMinimumVersion(10, 15, 0);
         }
