fe0435ac4e94a1007a2d96148c3c1b36938ededa
Max Henkel <max@maxhenkel.de>
2023-03-27 18:10:08 +0200

    Use configbuilder for Forge

--- fabric/src/main/java/de/maxhenkel/voicechat/FabricVoicechatClientMod.java
+++ fabric/src/main/java/de/maxhenkel/voicechat/FabricVoicechatClientMod.java
@@ -1,28 +1,15 @@
 package de.maxhenkel.voicechat;
 
-import com.sun.jna.Platform;
-import de.maxhenkel.configbuilder.ConfigBuilder;
-import de.maxhenkel.voicechat.config.FabricClientConfig;
-import de.maxhenkel.voicechat.macos.VersionCheck;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.Minecraft;
 
 @Environment(EnvType.CLIENT)
 public class FabricVoicechatClientMod extends VoicechatClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        CLIENT_CONFIG = ConfigBuilder.build(Minecraft.getInstance().gameDirectory.toPath().resolve("config").resolve(Voicechat.MODID).resolve("voicechat-client.properties"), true, FabricClientConfig::new);
         initializeClient();
-
-        if (Platform.isMac() && !CLIENT_CONFIG.javaMicrophoneImplementation.get()) {
-            CLIENT_CONFIG.javaMicrophoneImplementation.set(true).save();
-        }
-        if (Platform.isMac() && CLIENT_CONFIG.useNatives.get() && !VersionCheck.isMacOSNativeCompatible()) {
-            CLIENT_CONFIG.useNatives.set(false).save();
-        }
     }
 
 }
--- forge/src/main/java/de/maxhenkel/voicechat/ForgeVoicechatClientMod.java
+++ forge/src/main/java/de/maxhenkel/voicechat/ForgeVoicechatClientMod.java
@@ -1,16 +1,13 @@
 package de.maxhenkel.voicechat;
 
-import com.sun.jna.Platform;
-import de.maxhenkel.voicechat.config.ForgeClientConfig;
+import de.maxhenkel.voicechat.config.ConfigMigrator;
 import de.maxhenkel.voicechat.gui.VoiceChatSettingsScreen;
 import de.maxhenkel.voicechat.integration.clothconfig.ClothConfig;
 import de.maxhenkel.voicechat.intercompatibility.ClientCompatibilityManager;
 import de.maxhenkel.voicechat.intercompatibility.ForgeClientCompatibilityManager;
-import de.maxhenkel.voicechat.macos.VersionCheck;
 import net.minecraftforge.client.ConfigScreenHandler;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -19,7 +16,6 @@ public class ForgeVoicechatClientMod extends VoicechatClient {
     public ForgeVoicechatClientMod() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(((ForgeClientCompatibilityManager) ClientCompatibilityManager.INSTANCE)::onRegisterKeyBinds);
-        CLIENT_CONFIG = ForgeVoicechatMod.registerConfig(ModConfig.Type.CLIENT, ForgeClientConfig::new);
     }
 
     public void clientSetup(FMLClientSetupEvent event) {
@@ -33,13 +29,12 @@ public class ForgeVoicechatClientMod extends VoicechatClient {
                 return new VoiceChatSettingsScreen(parent);
             }
         }));
+    }
 
-        if (Platform.isMac() && !CLIENT_CONFIG.javaMicrophoneImplementation.get()) {
-            CLIENT_CONFIG.javaMicrophoneImplementation.set(true).save();
-        }
-        if (Platform.isMac() && CLIENT_CONFIG.useNatives.get() && !VersionCheck.isMacOSNativeCompatible()) {
-            CLIENT_CONFIG.useNatives.set(false).save();
-        }
+    @Override
+    public void initializeConfigs() {
+        super.initializeConfigs();
+        ConfigMigrator.migrateClientConfig();
     }
 
 }
