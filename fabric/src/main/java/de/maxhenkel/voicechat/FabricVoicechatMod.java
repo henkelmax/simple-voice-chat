483d3804f715cd189ab2cf2adecf9e6e674fe5c6
Max Henkel <max@maxhenkel.de>
2023-03-27 18:10:08 +0200

    Use configbuilder for Forge

--- fabric/src/main/java/de/maxhenkel/voicechat/FabricVoicechatMod.java
+++ fabric/src/main/java/de/maxhenkel/voicechat/FabricVoicechatMod.java
@@ -1,19 +1,13 @@
 package de.maxhenkel.voicechat;
 
-import de.maxhenkel.configbuilder.ConfigBuilder;
-import de.maxhenkel.voicechat.config.FabricServerConfig;
 import de.maxhenkel.voicechat.integration.ViaVersionCompatibility;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.loader.api.FabricLoader;
 
 public class FabricVoicechatMod extends Voicechat implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        SERVER_CONFIG = ConfigBuilder.build(FabricLoader.getInstance().getConfigDir().resolve(MODID).resolve("voicechat-server.properties"), true, FabricServerConfig::new);
-
         initialize();
-
         ViaVersionCompatibility.register();
     }
 
