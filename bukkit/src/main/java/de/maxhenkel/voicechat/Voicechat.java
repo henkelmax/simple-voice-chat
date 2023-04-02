8e6bc4e0b49f35b1c6d2026f3eacb332ed685905
Max Henkel <max@maxhenkel.de>
2023-04-02 18:32:56 +0200

    Add Folia support Closes #558

--- bukkit/src/main/java/de/maxhenkel/voicechat/Voicechat.java
+++ bukkit/src/main/java/de/maxhenkel/voicechat/Voicechat.java
@@ -116,7 +116,7 @@ public final class Voicechat extends JavaPlugin {
         }
         System.setProperty("VOICECHAT_RELOADED", "true");
 
-        getServer().getScheduler().scheduleSyncDelayedTask(this, () -> {
+        compatibility.runTask(() -> {
             SERVER = new ServerVoiceEvents();
             PluginManager.instance().init();
             SERVER.init();
