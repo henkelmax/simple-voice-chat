057c8b2523cb80220281696332e0eb9cf8d4514b
Max Henkel <max@maxhenkel.de>
2023-04-10 16:43:51 +0200

    Don't use server-ip as bind address if it's a loopback address

--- bukkit/src/main/java/de/maxhenkel/voicechat/voice/server/Server.java
+++ bukkit/src/main/java/de/maxhenkel/voicechat/voice/server/Server.java
@@ -19,6 +19,7 @@ import javax.annotation.Nullable;
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import java.net.InetAddress;
 import java.security.InvalidKeyException;
 import java.security.SecureRandom;
 import java.util.Collection;
@@ -79,7 +80,17 @@ public class Server extends Thread {
                 try {
                     bindAddress = Voicechat.compatibility.getServerIp(server);
                     if (!bindAddress.trim().isEmpty()) {
-                        Voicechat.LOGGER.info("Using server-ip as bind address: {}", bindAddress);
+                        try {
+                            InetAddress address = InetAddress.getByName(bindAddress);
+                            if (address.isLoopbackAddress()) {
+                                bindAddress = "";
+                            } else {
+                                Voicechat.LOGGER.info("Using server-ip as bind address: {}", bindAddress);
+                            }
+                        } catch (Exception e) {
+                            Voicechat.LOGGER.warn("Invalid server-ip", e);
+                            bindAddress = "";
+                        }
                     }
                 } catch (Throwable t) {
                     Voicechat.LOGGER.warn("Failed to get server-ip from server.properties - binding to wildcard address", t);
