79b4dd3011182f6cb6740265e63b805c19b53381
Max Henkel <max@maxhenkel.de>
2023-04-21 09:11:18 +0200

    Backport to 1.12.2

--- common/src/main/java/de/maxhenkel/voicechat/plugins/impl/events/PlayerStateChangedEventImpl.java
+++ common/src/main/java/de/maxhenkel/voicechat/plugins/impl/events/PlayerStateChangedEventImpl.java
@@ -6,7 +6,7 @@ import de.maxhenkel.voicechat.api.events.PlayerStateChangedEvent;
 import de.maxhenkel.voicechat.plugins.impl.VoicechatConnectionImpl;
 import de.maxhenkel.voicechat.voice.common.PlayerState;
 import de.maxhenkel.voicechat.voice.server.Server;
-import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 
 import javax.annotation.Nullable;
 import java.util.UUID;
@@ -44,7 +44,7 @@ public class PlayerStateChangedEventImpl extends ServerEventImpl implements Play
             if (server == null) {
                 return null;
             }
-            ServerPlayer player = server.getServer().getPlayerList().getPlayer(state.getUuid());
+            EntityPlayerMP player = server.getServer().getPlayerList().getPlayerByUUID(state.getUuid());
             if (player == null) {
                 return null;
             }
