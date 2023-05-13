a8046c04c4cdf53e46a3e132383593b5e4c96fa0
Max Henkel <max@maxhenkel.de>
2023-05-13 12:42:00 +0200

    Improve audio sender API

--- api/src/main/java/de/maxhenkel/voicechat/api/audiosender/AudioSender.java
+++ api/src/main/java/de/maxhenkel/voicechat/api/audiosender/AudioSender.java
@@ -3,9 +3,13 @@ package de.maxhenkel.voicechat.api.audiosender;
 public interface AudioSender {
 
     /**
-     * @return a builder for a microphone packet
+     * This can be reused.
+     * <br/>
+     * Invoking this method multiple times, will always return the same instance.
+     *
+     * @return a microphone packet sender
      */
-    MicrophonePacketBuilder microphonePacketBuilder();
+    MicrophonePacketSender microphonePacketSender();
 
     /**
      * @return if a microphone packet can be sent. This will return false if the player has the mod installed or the {@link AudioSender} is not registered.
@@ -13,31 +17,24 @@ public interface AudioSender {
     boolean canSend();
 
     /**
-     * Flushes the audio sender.
-     * This method should be called when the sending of audio is stopped.
-     * You can still send audio after this method is called.
-     */
-    void flush();
-
-    /**
-     * This builder can be reused.
+     * This sender can be reused.
      * <br/>
      * <b>NOTE</b>: Some values are required to be set.
      */
-    public interface MicrophonePacketBuilder {
+    public interface MicrophonePacketSender {
         /**
          * This is required to be set!
          *
          * @param data the opus encoded audio data from the player
          * @return the builder
          */
-        MicrophonePacketBuilder opusEncodedData(byte[] data);
+        MicrophonePacketSender opusEncodedData(byte[] data);
 
         /**
          * @param whispering if the player should whisper
          * @return the builder
          */
-        MicrophonePacketBuilder whispering(boolean whispering);
+        MicrophonePacketSender whispering(boolean whispering);
 
         /**
          * Sets the sequence number of the packet.
@@ -47,14 +44,26 @@ public interface AudioSender {
          * @param sequenceNumber the sequence number (Must be >= 0)
          * @return the builder
          */
-        MicrophonePacketBuilder sequenceNumber(long sequenceNumber);
+        MicrophonePacketSender sequenceNumber(long sequenceNumber);
 
         /**
          * Acts as if the player has sent a microphone packet.
+         * <br/>
+         * <b>NOTE</b>: Calling this method will reset all values, meaning you need to set all required values again.
          *
          * @return <code>true</code> if the packet was sent, <code>false</code> if the sender does have the mod installed or the {@link AudioSender} is not registered
          */
         boolean send();
+
+        /**
+         * Resets the sequence number and indicates to clients that the current audio stream is paused/stopped.
+         * <br/>
+         * This method should be called when the sending of audio is paused or stopped.
+         * You can still send audio after this method is called.
+         * <br/>
+         * <b>NOTE</b>: Calling this method will reset all values, meaning you need to set all required values again.
+         */
+        boolean reset();
     }
 
 }
