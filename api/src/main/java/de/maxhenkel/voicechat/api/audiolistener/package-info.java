a8679a05390a4554e3146225a2e1e8996fa8d14c
Max Henkel <max@maxhenkel.de>
2023-05-22 19:13:18 +0200

    Add missing package infos to API

---
+++ api/src/main/java/de/maxhenkel/voicechat/api/audiolistener/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Utilities for listening to all audio a player would theoretically receive.
+ * <p>
+ * Can be obtained by calling {@link de.maxhenkel.voicechat.api.VoicechatServerApi#playerAudioListenerBuilder()}.
+ */
+package de.maxhenkel.voicechat.api.audiolistener;
---
+++ api/src/main/java/de/maxhenkel/voicechat/api/audiosender/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Utilities for simulating players sending audio without the mod.
+ * <p>
+ * Can be obtained by calling {@link de.maxhenkel.voicechat.api.VoicechatServerApi#createAudioSender(de.maxhenkel.voicechat.api.VoicechatConnection)}.
+ */
+package de.maxhenkel.voicechat.api.audiosender;
