e179f108898a2ce71e87829f144f653c9c3a236f
Max Henkel <max@maxhenkel.de>
2023-05-22 15:00:38 +0200

    Fix group in LeaveGroupEvent always null

--- api/src/main/java/de/maxhenkel/voicechat/api/events/LeaveGroupEvent.java
+++ api/src/main/java/de/maxhenkel/voicechat/api/events/LeaveGroupEvent.java
@@ -3,11 +3,14 @@ package de.maxhenkel.voicechat.api.events;
 import de.maxhenkel.voicechat.api.Group;
 import de.maxhenkel.voicechat.api.VoicechatConnection;
 
+import javax.annotation.Nullable;
+
 public interface LeaveGroupEvent extends GroupEvent {
 
     /**
-     * @return the group that was left
+     * @return the group that was left or <code>null</code> if the player was not in a group
      */
+    @Nullable
     Group getGroup();
 
     /**
