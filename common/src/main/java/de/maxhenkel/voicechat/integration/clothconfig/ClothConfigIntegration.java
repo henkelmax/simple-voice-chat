d1ac9565ef05f5af8b56f8dfd18e6dee1261535f
Max Henkel <max@maxhenkel.de>
2023-10-26 16:27:23 +0200

    Add cloth config descriptions

--- common/src/main/java/de/maxhenkel/voicechat/integration/clothconfig/ClothConfigIntegration.java
+++ common/src/main/java/de/maxhenkel/voicechat/integration/clothconfig/ClothConfigIntegration.java
@@ -26,43 +26,48 @@ public class ClothConfigIntegration {
 
         ConfigCategory general = builder.getOrCreateCategory(new TranslationTextComponent("cloth_config.voicechat.category.general"));
 
-        general.addEntry(fromConfigEntry(entryBuilder, new TranslationTextComponent("cloth_config.voicechat.config.recording_destination"), VoicechatClient.CLIENT_CONFIG.recordingDestination));
-        general.addEntry(fromConfigEntry(entryBuilder, new TranslationTextComponent("cloth_config.voicechat.config.run_local_server"), VoicechatClient.CLIENT_CONFIG.runLocalServer));
-        general.addEntry(fromConfigEntry(entryBuilder, new TranslationTextComponent("cloth_config.voicechat.config.offline_player_volume_adjustment"), VoicechatClient.CLIENT_CONFIG.offlinePlayerVolumeAdjustment));
-        general.addEntry(fromConfigEntry(entryBuilder, new TranslationTextComponent("cloth_config.voicechat.config.freecam_support"), VoicechatClient.CLIENT_CONFIG.freecamSupport));
-        general.addEntry(fromConfigEntry(entryBuilder, new TranslationTextComponent("cloth_config.voicechat.config.mute_on_join"), VoicechatClient.CLIENT_CONFIG.muteOnJoin));
+        general.addEntry(fromConfigEntry(entryBuilder, VoicechatClient.CLIENT_CONFIG.recordingDestination));
+        general.addEntry(fromConfigEntry(entryBuilder, VoicechatClient.CLIENT_CONFIG.runLocalServer));
+        general.addEntry(fromConfigEntry(entryBuilder, VoicechatClient.CLIENT_CONFIG.offlinePlayerVolumeAdjustment));
+        general.addEntry(fromConfigEntry(entryBuilder, VoicechatClient.CLIENT_CONFIG.freecamSupport));
+        general.addEntry(fromConfigEntry(entryBuilder, VoicechatClient.CLIENT_CONFIG.muteOnJoin));
 
         ConfigCategory audio = builder.getOrCreateCategory(new TranslationTextComponent("cloth_config.voicechat.category.audio"));
-        audio.addEntry(fromConfigEntry(entryBuilder, new TranslationTextComponent("cloth_config.voicechat.config.audio_packet_threshold"), VoicechatClient.CLIENT_CONFIG.audioPacketThreshold));
-        audio.addEntry(fromConfigEntry(entryBuilder, new TranslationTextComponent("cloth_config.voicechat.config.deactivation_delay"), VoicechatClient.CLIENT_CONFIG.deactivationDelay));
-        audio.addEntry(fromConfigEntry(entryBuilder, new TranslationTextComponent("cloth_config.voicechat.config.output_buffer_size"), VoicechatClient.CLIENT_CONFIG.outputBufferSize));
+        audio.addEntry(fromConfigEntry(entryBuilder, VoicechatClient.CLIENT_CONFIG.audioPacketThreshold));
+        audio.addEntry(fromConfigEntry(entryBuilder, VoicechatClient.CLIENT_CONFIG.deactivationDelay));
+        audio.addEntry(fromConfigEntry(entryBuilder, VoicechatClient.CLIENT_CONFIG.outputBufferSize));
 
         ConfigCategory hudIcons = builder.getOrCreateCategory(new TranslationTextComponent("cloth_config.voicechat.category.hud_icons"));
-        hudIcons.addEntry(fromConfigEntry(entryBuilder, new TranslationTextComponent("cloth_config.voicechat.config.hud_icon_scale"), VoicechatClient.CLIENT_CONFIG.hudIconScale));
-        hudIcons.addEntry(fromConfigEntry(entryBuilder, new TranslationTextComponent("cloth_config.voicechat.config.hud_icon_x"), VoicechatClient.CLIENT_CONFIG.hudIconPosX));
-        hudIcons.addEntry(fromConfigEntry(entryBuilder, new TranslationTextComponent("cloth_config.voicechat.config.hud_icon_y"), VoicechatClient.CLIENT_CONFIG.hudIconPosY));
+        hudIcons.addEntry(fromConfigEntry(entryBuilder, VoicechatClient.CLIENT_CONFIG.hudIconScale));
+        hudIcons.addEntry(fromConfigEntry(entryBuilder, VoicechatClient.CLIENT_CONFIG.hudIconPosX));
+        hudIcons.addEntry(fromConfigEntry(entryBuilder, VoicechatClient.CLIENT_CONFIG.hudIconPosY));
 
         ConfigCategory groupIcons = builder.getOrCreateCategory(new TranslationTextComponent("cloth_config.voicechat.category.group_chat_icons"));
         groupIcons.addEntry(entryBuilder
                 .startEnumSelector(new TranslationTextComponent("cloth_config.voicechat.config.group_player_icon_orientation"), GroupPlayerIconOrientation.class, VoicechatClient.CLIENT_CONFIG.groupPlayerIconOrientation.get())
+                .setTooltip(new TranslationTextComponent("cloth_config.voicechat.config.group_player_icon_orientation.description"))
                 .setDefaultValue(VoicechatClient.CLIENT_CONFIG.groupPlayerIconOrientation::getDefault)
                 .setSaveConsumer(e -> VoicechatClient.CLIENT_CONFIG.groupPlayerIconOrientation.set(e).save())
                 .build()
         );
-        groupIcons.addEntry(fromConfigEntry(entryBuilder, new TranslationTextComponent("cloth_config.voicechat.config.group_hud_icon_scale"), VoicechatClient.CLIENT_CONFIG.groupHudIconScale));
-        groupIcons.addEntry(fromConfigEntry(entryBuilder, new TranslationTextComponent("cloth_config.voicechat.config.group_player_icon_pos_x"), VoicechatClient.CLIENT_CONFIG.groupPlayerIconPosX));
-        groupIcons.addEntry(fromConfigEntry(entryBuilder, new TranslationTextComponent("cloth_config.voicechat.config.group_player_icon_pos_y"), VoicechatClient.CLIENT_CONFIG.groupPlayerIconPosY));
-        groupIcons.addEntry(fromConfigEntry(entryBuilder, new TranslationTextComponent("cloth_config.voicechat.config.show_own_group_icon"), VoicechatClient.CLIENT_CONFIG.showOwnGroupIcon));
+        groupIcons.addEntry(fromConfigEntry(entryBuilder, VoicechatClient.CLIENT_CONFIG.groupHudIconScale));
+        groupIcons.addEntry(fromConfigEntry(entryBuilder, VoicechatClient.CLIENT_CONFIG.groupPlayerIconPosX));
+        groupIcons.addEntry(fromConfigEntry(entryBuilder, VoicechatClient.CLIENT_CONFIG.groupPlayerIconPosY));
+        groupIcons.addEntry(fromConfigEntry(entryBuilder, VoicechatClient.CLIENT_CONFIG.showOwnGroupIcon));
 
         builder.getOrCreateCategory(OTHER_SETTINGS);
         return builder.build();
     }
 
-    protected static <T> AbstractConfigListEntry<T> fromConfigEntry(ConfigEntryBuilder entryBuilder, ITextComponent name, ConfigEntry<T> entry) {
+    protected static <T> AbstractConfigListEntry<T> fromConfigEntry(ConfigEntryBuilder entryBuilder, ConfigEntry<T> entry) {
+        Component name = new TranslationTextComponent(String.format("cloth_config.voicechat.config.%s", entry.getKey()));
+        Component description = new TranslationTextComponent(String.format("cloth_config.voicechat.config.%s.description", entry.getKey()));
+
         if (entry instanceof DoubleConfigEntry) {
             DoubleConfigEntry e = (DoubleConfigEntry) entry;
             return (AbstractConfigListEntry<T>) entryBuilder
                     .startDoubleField(name, e.get())
+                    .setTooltip(description)
                     .setMin(e.getMin())
                     .setMax(e.getMax())
                     .setDefaultValue(e::getDefault)
@@ -75,6 +80,7 @@ public class ClothConfigIntegration {
             IntegerConfigEntry e = (IntegerConfigEntry) entry;
             return (AbstractConfigListEntry<T>) entryBuilder
                     .startIntField(name, e.get())
+                    .setTooltip(description)
                     .setMin(e.getMin())
                     .setMax(e.getMax())
                     .setDefaultValue(e::getDefault)
@@ -84,6 +90,7 @@ public class ClothConfigIntegration {
             BooleanConfigEntry e = (BooleanConfigEntry) entry;
             return (AbstractConfigListEntry<T>) entryBuilder
                     .startBooleanToggle(name, e.get())
+                    .setTooltip(description)
                     .setDefaultValue(e::getDefault)
                     .setSaveConsumer(d -> e.set(d).save())
                     .build();
@@ -91,6 +98,7 @@ public class ClothConfigIntegration {
             StringConfigEntry e = (StringConfigEntry) entry;
             return (AbstractConfigListEntry<T>) entryBuilder
                     .startStrField(name, e.get())
+                    .setTooltip(description)
                     .setDefaultValue(e::getDefault)
                     .setSaveConsumer(d -> e.set(d).save())
                     .build();
