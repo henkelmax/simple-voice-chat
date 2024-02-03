fa75a61aa143e7f6e78b5253d14c5201af2f74f9
Max Henkel <max@maxhenkel.de>
2024-02-03 13:23:02 +0100

    Backport changes

--- common/src/main/java/de/maxhenkel/voicechat/gui/onboarding/IntroductionOnboardingScreen.java
+++ common/src/main/java/de/maxhenkel/voicechat/gui/onboarding/IntroductionOnboardingScreen.java
@@ -38,8 +38,8 @@ public class IntroductionOnboardingScreen extends OnboardingScreenBase {
     }
 
     @Override
-    public void renderBackground(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTicks) {
-        super.renderBackground(guiGraphics, mouseX, mouseY, partialTicks);
+    public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTicks) {
+        super.render(guiGraphics, mouseX, mouseY, partialTicks);
         renderTitle(guiGraphics, TITLE);
         renderMultilineText(guiGraphics, DESCRIPTION);
     }
