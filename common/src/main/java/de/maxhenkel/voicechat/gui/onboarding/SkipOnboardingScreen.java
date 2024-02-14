b570469883e2353a1aa858976c3ff1589e12f198
Max Henkel <max@maxhenkel.de>
2024-02-14 15:43:16 +0100

    Add confirm skip setup screen

---
+++ common/src/main/java/de/maxhenkel/voicechat/gui/onboarding/SkipOnboardingScreen.java
@@ -0,0 +1,41 @@
+package de.maxhenkel.voicechat.gui.onboarding;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import de.maxhenkel.voicechat.intercompatibility.CommonCompatibilityManager;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+
+import javax.annotation.Nullable;
+
+public class SkipOnboardingScreen extends OnboardingScreenBase {
+
+    private static final Component TITLE = Component.translatable("message.voicechat.onboarding.skip.title", CommonCompatibilityManager.INSTANCE.getModName()).withStyle(ChatFormatting.BOLD);
+    private static final Component DESCRIPTION = Component.translatable("message.voicechat.onboarding.skip.description");
+    private static final Component CONFIRM = Component.translatable("message.voicechat.onboarding.confirm");
+
+    public SkipOnboardingScreen(@Nullable Screen previous) {
+        super(TITLE, previous);
+    }
+
+    @Override
+    protected void init() {
+        super.init();
+
+        addPositiveButton(CONFIRM, button -> OnboardingManager.finishOnboarding());
+        addBackOrCancelButton();
+    }
+
+    @Override
+    public Screen getNextScreen() {
+        return previous;
+    }
+
+    @Override
+    public void render(PoseStack poseStack, int mouseX, int mouseY, float partialTicks) {
+        super.render(poseStack, mouseX, mouseY, partialTicks);
+        renderTitle(poseStack, TITLE);
+        renderMultilineText(poseStack, DESCRIPTION);
+    }
+
+}
