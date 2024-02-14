3d4868d2eb1992bd6ef02ae4f9994ee67f2c5a88
Max Henkel <max@maxhenkel.de>
2024-02-14 15:46:51 +0100

    Backport changes

--- common/src/main/java/de/maxhenkel/voicechat/gui/onboarding/SkipOnboardingScreen.java
+++ common/src/main/java/de/maxhenkel/voicechat/gui/onboarding/SkipOnboardingScreen.java
@@ -5,14 +5,15 @@ import de.maxhenkel.voicechat.intercompatibility.CommonCompatibilityManager;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
 
 import javax.annotation.Nullable;
 
 public class SkipOnboardingScreen extends OnboardingScreenBase {
 
-    private static final Component TITLE = Component.translatable("message.voicechat.onboarding.skip.title", CommonCompatibilityManager.INSTANCE.getModName()).withStyle(ChatFormatting.BOLD);
-    private static final Component DESCRIPTION = Component.translatable("message.voicechat.onboarding.skip.description");
-    private static final Component CONFIRM = Component.translatable("message.voicechat.onboarding.confirm");
+    private static final Component TITLE = new TranslatableComponent("message.voicechat.onboarding.skip.title", CommonCompatibilityManager.INSTANCE.getModName()).withStyle(ChatFormatting.BOLD);
+    private static final Component DESCRIPTION = new TranslatableComponent("message.voicechat.onboarding.skip.description");
+    private static final Component CONFIRM = new TranslatableComponent("message.voicechat.onboarding.confirm");
 
     public SkipOnboardingScreen(@Nullable Screen previous) {
         super(TITLE, previous);
