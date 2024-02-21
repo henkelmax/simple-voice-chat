063214b599950efbe321c1c1d2db4d03928cc3ee
Max Henkel <max@maxhenkel.de>
2024-02-21 12:41:54 +0100

    Fix potential null pointer exception

--- common/src/main/java/de/maxhenkel/voicechat/voice/client/KeyEvents.java
+++ common/src/main/java/de/maxhenkel/voicechat/voice/client/KeyEvents.java
@@ -1,5 +1,6 @@
 package de.maxhenkel.voicechat.voice.client;
 
+import de.maxhenkel.voicechat.Voicechat;
 import de.maxhenkel.voicechat.VoicechatClient;
 import de.maxhenkel.voicechat.gui.VoiceChatScreen;
 import de.maxhenkel.voicechat.gui.VoiceChatSettingsScreen;
@@ -138,14 +139,19 @@ public class KeyEvents {
 
     private boolean checkConnected() {
         if (ClientManager.getClient() == null || ClientManager.getClient().getConnection() == null || !ClientManager.getClient().getConnection().isInitialized()) {
-            sendUnavailableMessage();
+            sendNotConnectedMessage();
             return false;
         }
         return true;
     }
 
-    private void sendUnavailableMessage() {
-        minecraft.player.sendStatusMessage(new TextComponentTranslation("message.voicechat.voice_chat_not_connected"), true);
+    private void sendNotConnectedMessage() {
+        EntityPlayerSP player = minecraft.player;
+        if (player == null) {
+            Voicechat.LOGGER.warn("Voice chat not connected");
+            return;
+        }
+        player.sendStatusMessage(new TextComponentTranslation("message.voicechat.voice_chat_not_connected"), true);
     }
 
 }
