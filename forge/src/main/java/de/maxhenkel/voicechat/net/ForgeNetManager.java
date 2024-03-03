64f876524a64c8ee210fde61d99db9132de1023b
Max Henkel <max@maxhenkel.de>
2024-03-03 10:56:13 +0100

    Improve Forge networking

--- forge/src/main/java/de/maxhenkel/voicechat/net/ForgeNetManager.java
+++ forge/src/main/java/de/maxhenkel/voicechat/net/ForgeNetManager.java
@@ -38,6 +38,7 @@ public class ForgeNetManager extends NetManager {
                         T packet = packetType.getDeclaredConstructor().newInstance();
                         packet.fromBytes(event.getPayload());
                         c.onServerPacket(context.getSender().server, context.getSender(), context.getSender().connection, packet);
+                        context.setPacketHandled(true);
                     } catch (Exception e) {
                         Voicechat.LOGGER.error("Failed to process packet", e);
                     }
@@ -49,6 +50,7 @@ public class ForgeNetManager extends NetManager {
                         T packet = packetType.getDeclaredConstructor().newInstance();
                         packet.fromBytes(event.getPayload());
                         onClientPacket(c, packet);
+                        context.setPacketHandled(true);
                     } catch (Exception e) {
                         Voicechat.LOGGER.error("Failed to process packet", e);
                     }
