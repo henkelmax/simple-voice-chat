bcec8e6134aee006f4e34d3cf5b178c1a165c10e
NilaTheDragon <nilathedragon@pm.me>
2024-03-12 19:02:54 +0100

    Implement Waterfall/Bungeecord

---
+++ waterfall/src/main/java/de/maxhenkel/voicechat/BungeecordPlugin.java
@@ -0,0 +1,23 @@
+package de.maxhenkel.voicechat;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class BungeecordPlugin extends Plugin {
+
+    private SimpleVoiceChatBungeecord simpleVoiceChatBungeecord;
+
+    @Override
+    public void onEnable() {
+        simpleVoiceChatBungeecord = new SimpleVoiceChatBungeecord(this);
+        this.getProxy().getPluginManager().registerListener(this, simpleVoiceChatBungeecord);
+        simpleVoiceChatBungeecord.onProxyInitialization();
+    }
+
+    @Override
+    public void onDisable() {
+        if (simpleVoiceChatBungeecord == null) return;
+        this.getProxy().getPluginManager().unregisterListener(simpleVoiceChatBungeecord);
+        simpleVoiceChatBungeecord.onProxyShutdown();
+        simpleVoiceChatBungeecord = null;
+    }
+}
