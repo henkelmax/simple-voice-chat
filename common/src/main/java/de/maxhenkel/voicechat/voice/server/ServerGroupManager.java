f9aa14a703cc27ebecdd48c718fc1eb793273231
Max Henkel <max@maxhenkel.de>
2024-09-17 09:21:47 +0200

    Fix potential incompatibility with other mods Closes #739

--- common/src/main/java/de/maxhenkel/voicechat/voice/server/ServerGroupManager.java
+++ common/src/main/java/de/maxhenkel/voicechat/voice/server/ServerGroupManager.java
@@ -46,7 +46,7 @@ public class ServerGroupManager {
                 return;
             }
             if (!Voicechat.GROUP_REGEX.matcher(packet.getName()).matches()) {
-                Voicechat.LOGGER.warn("Player {} tried to create a group with an invalid name: {}", player.getDisplayName().getString(), packet.getName());
+                Voicechat.LOGGER.warn("Player {} tried to create a group with an invalid name: {}", player.getName().getString(), packet.getName());
                 return;
             }
             addGroup(new Group(UUID.randomUUID(), packet.getName(), packet.getPassword(), false, false, packet.getType()), player);
@@ -57,7 +57,7 @@ public class ServerGroupManager {
     }
 
     public void onPlayerCompatibilityCheckSucceeded(ServerPlayer player) {
-        Voicechat.LOGGER.debug("Synchronizing {} groups with {}", groups.size(), player.getDisplayName().getString());
+        Voicechat.LOGGER.debug("Synchronizing {} groups with {}", groups.size(), player.getName().getString());
         for (Group category : groups.values()) {
             broadcastAddGroup(category);
         }
