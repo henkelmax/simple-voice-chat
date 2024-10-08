9de0c2fba717a29425194f5ad04eb7101a7e2ae1
Greg-21 <34160838+Greg-21@users.noreply.github.com>
2024-10-08 16:10:14 +0200

    Revamped configuration file comments

--- common-proxy/src/main/java/de/maxhenkel/voicechat/config/ProxyConfig.java
+++ common-proxy/src/main/java/de/maxhenkel/voicechat/config/ProxyConfig.java
@@ -16,27 +16,29 @@ public class ProxyConfig {
 
         port = builder
                 .integerEntry("port", -1, -1, 65535,
-                        "The port of the voice chat proxy server",
-                        "Setting this to \"-1\" sets the port to the proxies port"
+                        "The port number to use for the voice chat communication.",
+                        "Audio packets are always transmitted via the UDP protocol on the port number",
+                        "specified here, independently of other networking used for the game server.",
+                        "Set this to '-1' to use the same port number as the one used by the proxy."
                 );
 
         bindAddress = builder
                 .stringEntry("bind_address", "",
-                        "The IP address to bind the voice chat proxy server on",
-                        "Leave empty to use the proxies bind address",
-                        "To bind to the wildcard address, use '*'"
+                        "The proxy IP address to bind the voice chat to",
+                        "Leave blank to use the proxy bind address",
+                        "To bind to the wildcard IP address, use '*'"
                 );
 
         voiceHost = builder
                 .stringEntry("voice_host", "",
-                        "The host name that clients should use to connect to the voice chat",
+                        "The hostname that clients should use to connect to the voice chat",
                         "This may also include a port, e.g. 'example.com:24454'",
-                        "Don't change this value if you don't know what you are doing"
+                        "Do NOT change this value if you don't know what you're doing"
                 );
 
         allowPings = builder
                 .booleanEntry("allow_pings", true,
-                        "If the voice chat proxy server should reply to pings"
+                        "If the voice chat proxy server should reply to external pings"
                 );
     }
 
