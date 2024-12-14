b37cd2a4066f4fee5d54ab7665c25b2a2b858a4b
Max Henkel <max@maxhenkel.de>
2024-12-14 15:38:14 +0100

    Backport changes

--- common-client/src/main/java/de/maxhenkel/voicechat/gui/group/JoinGroupScreen.java
+++ common-client/src/main/java/de/maxhenkel/voicechat/gui/group/JoinGroupScreen.java
@@ -4,13 +4,19 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import de.maxhenkel.voicechat.Voicechat;
 import de.maxhenkel.voicechat.gui.CreateGroupScreen;
+import de.maxhenkel.voicechat.gui.EnterPasswordScreen;
 import de.maxhenkel.voicechat.gui.widgets.ListScreenBase;
+import de.maxhenkel.voicechat.net.ClientServerNetManager;
+import de.maxhenkel.voicechat.net.JoinGroupPacket;
+import de.maxhenkel.voicechat.voice.common.ClientGroup;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.resources.sounds.SimpleSoundInstance;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.resources.sounds.SimpleSoundInstance;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.util.Mth;
 
 public class JoinGroupScreen extends ListScreenBase {
@@ -90,7 +96,7 @@ public class JoinGroupScreen extends ListScreenBase {
                 if (group.hasPassword()) {
                     minecraft.setScreen(new EnterPasswordScreen(group));
                 } else {
-                    NetManager.sendToServer(new JoinGroupPacket(group.getId(), null));
+                    ClientServerNetManager.sendToServer(new JoinGroupPacket(group.getId(), null));
                 }
                 return true;
             }
