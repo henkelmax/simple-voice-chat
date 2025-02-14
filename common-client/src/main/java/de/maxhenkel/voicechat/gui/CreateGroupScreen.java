dce3ceaaed10846dbb2f72840e46b301b6fcf8bb
Max Henkel <max@maxhenkel.de>
2025-02-14 11:10:13 +0100

    Adjust group screen text field sizes

--- common-client/src/main/java/de/maxhenkel/voicechat/gui/CreateGroupScreen.java
+++ common-client/src/main/java/de/maxhenkel/voicechat/gui/CreateGroupScreen.java
@@ -44,12 +44,12 @@ public class CreateGroupScreen extends VoiceChatScreenBase {
         clearWidgets();
         minecraft.keyboardHandler.setSendRepeatsToGui(true);
 
-        groupName = new EditBox(font, guiLeft + 7, guiTop + 30, xSize - 7 * 2, 14, TextComponent.EMPTY);
+        groupName = new EditBox(font, guiLeft + 7, guiTop + 31, xSize - 7 * 2, 12, TextComponent.EMPTY);
         groupName.setMaxLength(24);
         groupName.setFilter(s -> s.isEmpty() || Voicechat.GROUP_REGEX.matcher(s).matches());
         addRenderableWidget(groupName);
 
-        password = new EditBox(font, guiLeft + 7, guiTop + 56, xSize - 7 * 2, 14, TextComponent.EMPTY);
+        password = new EditBox(font, guiLeft + 7, guiTop + 57, xSize - 7 * 2, 12, TextComponent.EMPTY);
         password.setMaxLength(32);
         password.setFilter(s -> s.isEmpty() || Voicechat.GROUP_REGEX.matcher(s).matches());
         addRenderableWidget(password);
