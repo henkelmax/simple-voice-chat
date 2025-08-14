560a2421ee3bc43d3d3c8fee48a21e5dcefa6160
Max Henkel <max@maxhenkel.de>
2025-08-14 15:53:22 +0200

    Clean up audio conversion for bukkit

--- bukkit/src/main/java/de/maxhenkel/voicechat/voice/common/AudioUtils.java
+++ bukkit/src/main/java/de/maxhenkel/voicechat/voice/common/AudioUtils.java
@@ -1,26 +1,26 @@
 package de.maxhenkel.voicechat.voice.common;
 
+import java.nio.ByteBuffer;
+import java.nio.ShortBuffer;
+
 public class AudioUtils {
 
     public static short[] bytesToShorts(byte[] bytes) {
         if (bytes.length % 2 != 0) {
             throw new IllegalArgumentException("Input bytes need to be divisible by 2");
         }
-        short[] data = new short[bytes.length / 2];
-        for (int i = 0; i < bytes.length; i += 2) {
-            data[i / 2] = bytesToShort(bytes[i], bytes[i + 1]);
-        }
-        return data;
+        ShortBuffer sb = ByteBuffer.wrap(bytes).asShortBuffer();
+        short[] out = new short[sb.remaining()];
+        sb.get(out);
+        return out;
     }
 
     public static byte[] shortsToBytes(short[] shorts) {
-        byte[] data = new byte[shorts.length * 2];
-        for (int i = 0; i < shorts.length; i++) {
-            byte[] split = shortToBytes(shorts[i]);
-            data[i * 2] = split[0];
-            data[i * 2 + 1] = split[1];
+        ByteBuffer bb = ByteBuffer.allocate(shorts.length * 2);
+        for (short s : shorts) {
+            bb.putShort(s);
         }
-        return data;
+        return bb.array();
     }
 
     public static short bytesToShort(byte b1, byte b2) {
