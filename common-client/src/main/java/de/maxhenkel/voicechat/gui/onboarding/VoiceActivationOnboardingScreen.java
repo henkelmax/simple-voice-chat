5592dcff00818f9224661a81ce86b99695789073
Max Henkel <max@maxhenkel.de>
2025-08-14 11:58:13 +0200

    Add gain control to onboarding screen

--- common-client/src/main/java/de/maxhenkel/voicechat/gui/onboarding/VoiceActivationOnboardingScreen.java
+++ common-client/src/main/java/de/maxhenkel/voicechat/gui/onboarding/VoiceActivationOnboardingScreen.java
@@ -1,9 +1,7 @@
 package de.maxhenkel.voicechat.gui.onboarding;
 
-import de.maxhenkel.voicechat.gui.widgets.DenoiserButton;
-import de.maxhenkel.voicechat.gui.widgets.MicAmplificationSlider;
-import de.maxhenkel.voicechat.gui.widgets.MicTestButton;
-import de.maxhenkel.voicechat.gui.widgets.VoiceActivationSlider;
+import de.maxhenkel.voicechat.gui.widgets.*;
+import de.maxhenkel.voicechat.voice.client.AutomaticGainControl;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.Style;
@@ -30,7 +28,12 @@ public class VoiceActivationOnboardingScreen extends OnboardingScreenBase {
         int bottom = guiTop + contentHeight - PADDING * 3 - BUTTON_HEIGHT * 2;
         int space = BUTTON_HEIGHT + SMALL_PADDING;
 
-        addButton(new MicAmplificationSlider(0, guiLeft, bottom - space * 2, contentWidth, BUTTON_HEIGHT));
+        boolean agc = AutomaticGainControl.canUseAgc();
+        MicAmplificationSlider micAmp = new MicAmplificationSlider(0, guiLeft + (agc ? 80 + 1 : 0), bottom - space * 2, contentWidth - (agc ? 80 : 0) - 1, BUTTON_HEIGHT);
+        if (agc) {
+            addButton(new AgcButton(guiLeft, bottom - space * 2, 80, BUTTON_HEIGHT, active -> micAmp.active = !active));
+        }
+        addButton(micAmp);
         addButton(new DenoiserButton(1, guiLeft, bottom - space, contentWidth, BUTTON_HEIGHT));
 
         slider = new VoiceActivationSlider(2, guiLeft + 20 + SMALL_PADDING, bottom, contentWidth - 20 - SMALL_PADDING, BUTTON_HEIGHT);
