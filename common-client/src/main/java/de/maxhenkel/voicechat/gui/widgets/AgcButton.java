df17e95f3851b622a75dbd7c3748603ff7ef6078
Max Henkel <max@maxhenkel.de>
2025-08-14 13:42:15 +0200

    Backport changes

--- common-client/src/main/java/de/maxhenkel/voicechat/gui/widgets/AgcButton.java
+++ common-client/src/main/java/de/maxhenkel/voicechat/gui/widgets/AgcButton.java
@@ -3,19 +3,20 @@ package de.maxhenkel.voicechat.gui.widgets;
 import de.maxhenkel.voicechat.VoicechatClient;
 import de.maxhenkel.voicechat.voice.client.AutomaticGainControl;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
 
 import java.util.function.Consumer;
 
 public class AgcButton extends BooleanConfigButton {
 
-    private static final Component AUTO = Component.translatable("message.voicechat.gain.auto");
-    private static final Component MANUAL = Component.translatable("message.voicechat.gain.manual");
+    private static final Component AUTO = new TranslatableComponent("message.voicechat.gain.auto");
+    private static final Component MANUAL = new TranslatableComponent("message.voicechat.gain.manual");
 
     private final Consumer<Boolean> onChange;
 
     public AgcButton(int x, int y, int width, int height, Consumer<Boolean> onChange) {
         super(x, y, width, height, VoicechatClient.CLIENT_CONFIG.agc, enabled -> {
-            return Component.translatable("message.voicechat.gain", enabled ? AUTO : MANUAL);
+            return new TranslatableComponent("message.voicechat.gain", enabled ? AUTO : MANUAL);
         });
         this.onChange = onChange;
         if (!AutomaticGainControl.canUseAgc()) {
