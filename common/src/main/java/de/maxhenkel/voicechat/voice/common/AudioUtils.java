4e0f4eb3a8b46448a41437e5692f497b69663796
Max Henkel <max@maxhenkel.de>
2025-08-14 12:09:18 +0200

    Clean up audio conversion

--- common/src/main/java/de/maxhenkel/voicechat/voice/common/AudioUtils.java
+++ common/src/main/java/de/maxhenkel/voicechat/voice/common/AudioUtils.java
@@ -1,5 +1,8 @@
 package de.maxhenkel.voicechat.voice.common;
 
+import java.nio.ByteBuffer;
+import java.nio.ShortBuffer;
+
 public class AudioUtils {
 
     public static final int SAMPLE_RATE = 48000;
@@ -11,29 +14,18 @@ public class AudioUtils {
         if (bytes.length % 2 != 0) {
             throw new IllegalArgumentException("Input bytes need to be divisible by 2");
         }
-        short[] data = new short[bytes.length / 2];
-        for (int i = 0; i < bytes.length; i += 2) {
-            data[i / 2] = bytesToShort(bytes[i], bytes[i + 1]);
-        }
-        return data;
+        ShortBuffer sb = ByteBuffer.wrap(bytes).asShortBuffer();
+        short[] out = new short[sb.remaining()];
+        sb.get(out);
+        return out;
     }
 
     public static byte[] shortsToBytes(short[] shorts) {
-        byte[] data = new byte[shorts.length * 2];
-        for (int i = 0; i < shorts.length; i++) {
-            byte[] split = shortToBytes(shorts[i]);
-            data[i * 2] = split[0];
-            data[i * 2 + 1] = split[1];
+        ByteBuffer bb = ByteBuffer.allocate(shorts.length * 2);
+        for (short s : shorts) {
+            bb.putShort(s);
         }
-        return data;
-    }
-
-    public static short bytesToShort(byte b1, byte b2) {
-        return (short) (((b2 & 0xFF) << 8) | (b1 & 0xFF));
-    }
-
-    public static byte[] shortToBytes(short s) {
-        return new byte[]{(byte) (s & 0xFF), (byte) ((s >> 8) & 0xFF)};
+        return bb.array();
     }
 
     private static final float FLOAT_SHORT_SCALE = Short.MAX_VALUE;
