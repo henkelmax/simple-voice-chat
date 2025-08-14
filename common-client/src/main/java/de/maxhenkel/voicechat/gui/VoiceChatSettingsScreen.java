7c9199984b6b0015a0df4320785a4bc08afbc863
Max Henkel <max@maxhenkel.de>
2025-08-14 19:50:35 +0200

    Fix speaker button alignment

--- common-client/src/main/java/de/maxhenkel/voicechat/gui/VoiceChatSettingsScreen.java
+++ common-client/src/main/java/de/maxhenkel/voicechat/gui/VoiceChatSettingsScreen.java
@@ -98,10 +98,10 @@ public class VoiceChatSettingsScreen extends VoiceChatScreenBase {
             }));
             y += 21;
         }
-        addRenderableWidget(new Button(guiLeft + 10, y, xSize / 2 - 15, 20, SELECT_MICROPHONE, button -> {
+        addRenderableWidget(new Button(guiLeft + 10, y, (xSize - 20) / 2 - 1, 20, SELECT_MICROPHONE, button -> {
             minecraft.setScreen(new SelectMicrophoneScreen(this));
         }));
-        addRenderableWidget(new Button(guiLeft + xSize / 2 + 1, y, (xSize - 20) / 2 - 1, 20, SELECT_SPEAKER, button -> {
+        addRenderableWidget(new Button(guiLeft + xSize / 2 + 2, y, (xSize - 20) / 2 - 1, 20, SELECT_SPEAKER, button -> {
             minecraft.setScreen(new SelectSpeakerScreen(this));
         }));
         y += 21;
