f6b7810f4284823467cbe34ae918f6174722dc25
Max Henkel <max@maxhenkel.de>
2025-08-16 13:55:12 +0200

    Update javadocs for static audio channel

--- api/src/main/java/de/maxhenkel/voicechat/api/audiochannel/StaticAudioChannel.java
+++ api/src/main/java/de/maxhenkel/voicechat/api/audiochannel/StaticAudioChannel.java
@@ -4,10 +4,23 @@ import de.maxhenkel.voicechat.api.VoicechatConnection;
 
 public interface StaticAudioChannel extends AudioChannel {
 
+    /**
+     * Adds a target to the audio channel.
+     *
+     * @param target the target to add
+     */
     void addTarget(VoicechatConnection target);
 
+    /**
+     * Removes a target from the audio channel.
+     *
+     * @param target the target to remove
+     */
     void removeTarget(VoicechatConnection target);
 
+    /**
+     * Removes all targets from the audio channel.
+     */
     void clearTargets();
 
 }
