ba035db4c0487cbfa69ba2f82887d4b54fdfbe34
Max Henkel <max@maxhenkel.de>
2025-08-16 13:31:09 +0200

    Allow multiple targets for static audio channels

--- api/src/main/java/de/maxhenkel/voicechat/api/audiochannel/StaticAudioChannel.java
+++ api/src/main/java/de/maxhenkel/voicechat/api/audiochannel/StaticAudioChannel.java
@@ -1,5 +1,13 @@
 package de.maxhenkel.voicechat.api.audiochannel;
 
+import de.maxhenkel.voicechat.api.VoicechatConnection;
+
 public interface StaticAudioChannel extends AudioChannel {
+
+    void addTarget(VoicechatConnection target);
+
+    void removeTarget(VoicechatConnection target);
+
+    void clearTargets();
 
 }
