fa07562aab9950fe8a41f0347c5d881e9fb0f1d4
Max Henkel <max@maxhenkel.de>
2025-08-16 17:34:56 +0200

    Replace `whisper_distance_multiplier` and `crouch_distance_multiplier` with `whisper_distance` and add `VoiceDistanceEvent` to the API

---
+++ common/src/main/java/de/maxhenkel/voicechat/plugins/impl/events/VoiceDistanceEventImpl.java
@@ -0,0 +1,40 @@
+package de.maxhenkel.voicechat.plugins.impl.events;
+
+import de.maxhenkel.voicechat.api.VoicechatConnection;
+import de.maxhenkel.voicechat.api.events.VoiceDistanceEvent;
+import de.maxhenkel.voicechat.api.packets.MicrophonePacket;
+import org.jetbrains.annotations.Nullable;
+
+public class VoiceDistanceEventImpl extends ServerEventImpl implements VoiceDistanceEvent {
+
+    private final MicrophonePacket packet;
+    private final VoicechatConnection senderConnection;
+    private float distance;
+
+    public VoiceDistanceEventImpl(MicrophonePacket packet, VoicechatConnection senderConnection, float distance) {
+        this.packet = packet;
+        this.senderConnection = senderConnection;
+        this.distance = distance;
+    }
+
+    @Override
+    public MicrophonePacket getPacket() {
+        return packet;
+    }
+
+    @Nullable
+    @Override
+    public VoicechatConnection getSenderConnection() {
+        return senderConnection;
+    }
+
+    @Override
+    public float getDistance() {
+        return distance;
+    }
+
+    @Override
+    public void setDistance(float distance) {
+        this.distance = distance;
+    }
+}
