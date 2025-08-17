bae18aca7e374a61711676a3b6aa8442dbc58647
Max Henkel <max@maxhenkel.de>
2025-08-17 16:52:23 +0200

    Add remove state packet and rework state manager

---
+++ common/src/main/java/de/maxhenkel/voicechat/net/RemovePlayerStatePacket.java
@@ -0,0 +1,43 @@
+package de.maxhenkel.voicechat.net;
+
+import de.maxhenkel.voicechat.Voicechat;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.UUID;
+
+public class RemovePlayerStatePacket implements Packet<RemovePlayerStatePacket> {
+
+    public static final Type<RemovePlayerStatePacket> REMOVE_PLAYER_STATE = new Type<>(ResourceLocation.fromNamespaceAndPath(Voicechat.MODID, "remove_state"));
+
+    private UUID id;
+
+    public RemovePlayerStatePacket() {
+
+    }
+
+    public RemovePlayerStatePacket(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    @Override
+    public RemovePlayerStatePacket fromBytes(FriendlyByteBuf buf) {
+        id = buf.readUUID();
+        return this;
+    }
+
+    @Override
+    public void toBytes(FriendlyByteBuf buf) {
+        buf.writeUUID(id);
+    }
+
+    @Override
+    public Type<RemovePlayerStatePacket> type() {
+        return REMOVE_PLAYER_STATE;
+    }
+
+}
