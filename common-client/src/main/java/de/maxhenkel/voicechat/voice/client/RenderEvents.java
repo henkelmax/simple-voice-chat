6773368bdfdce28206d14ce8536bcc357bb7dfef
Max Henkel <max@maxhenkel.de>
2025-08-17 09:59:07 +0200

    Add `show_nametag_icons` and `show_hud_icons` config option

--- common-client/src/main/java/de/maxhenkel/voicechat/voice/client/RenderEvents.java
+++ common-client/src/main/java/de/maxhenkel/voicechat/voice/client/RenderEvents.java
@@ -48,6 +48,9 @@ public class RenderEvents {
         if (VoicechatClient.CLIENT_CONFIG.hideIcons.get()) {
             return;
         }
+        if (!VoicechatClient.CLIENT_CONFIG.showHudIcons.get()) {
+            return;
+        }
 
         ClientPlayerStateManager manager = ClientManager.getPlayerStateManager();
         ClientVoicechat client = ClientManager.getClient();
@@ -108,6 +111,9 @@ public class RenderEvents {
             return;
         }
         if (VoicechatClient.CLIENT_CONFIG.hideIcons.get()) {
+            return;
+        }
+        if (!VoicechatClient.CLIENT_CONFIG.showNametagIcons.get()) {
             return;
         }
         if (!(entity instanceof Player player)) {
