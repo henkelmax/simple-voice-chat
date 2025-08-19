80349057d3bc4923d8dfaac9f4626efb3311d705
Max Henkel <max@maxhenkel.de>
2025-08-19 15:20:49 +0200

    Implement client config migration and enable noise suppression by default

--- common-client/src/main/java/de/maxhenkel/voicechat/VoicechatClient.java
+++ common-client/src/main/java/de/maxhenkel/voicechat/VoicechatClient.java
@@ -48,7 +48,7 @@ public abstract class VoicechatClient {
     }
 
     public void initializeConfigs() {
-        CLIENT_CONFIG = ConfigBuilder.builder(ClientConfig::new).path(Voicechat.getVoicechatConfigFolder().resolve("voicechat-client.properties")).build();
+        CLIENT_CONFIG = ConfigBuilder.builder(ClientConfig::new).path(Voicechat.getVoicechatConfigFolder().resolve("voicechat-client.properties")).migration(ClientConfig::migrate).build();
         PLAYER_VOLUME_CONFIG = new PlayerVolumeConfig(Voicechat.getVoicechatConfigFolder().resolve("player-volumes.properties"));
         CATEGORY_VOLUME_CONFIG = new CategoryVolumeConfig(Voicechat.getVoicechatConfigFolder().resolve("category-volumes.properties"));
         USERNAME_CACHE = new UsernameCache(Voicechat.getVoicechatConfigFolder().resolve("username-cache.json").toFile());
