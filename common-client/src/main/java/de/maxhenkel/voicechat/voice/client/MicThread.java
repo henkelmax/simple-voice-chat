6204e3fec7705e36f9c0f7827f04f8871fafeb06
Max Henkel <max@maxhenkel.de>
2025-08-20 16:21:22 +0200

    Fix deactivation delay not respected when testing the microphone

--- common-client/src/main/java/de/maxhenkel/voicechat/voice/client/MicThread.java
+++ common-client/src/main/java/de/maxhenkel/voicechat/voice/client/MicThread.java
@@ -70,10 +70,10 @@ public class MicThread extends Thread {
                 }
             }
             if (microphoneLocked || ClientManager.getPlayerStateManager().isDisabled()) {
-                microphoneProcessor.reset();
                 flushIfNeeded();
 
                 if (!microphoneLocked && ClientManager.getPlayerStateManager().isDisabled()) {
+                    microphoneProcessor.reset();
                     if (mic.isStarted()) {
                         mic.stop();
                     }
