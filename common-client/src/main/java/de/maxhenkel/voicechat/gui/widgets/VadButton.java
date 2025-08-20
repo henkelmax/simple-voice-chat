c7657ee5f4cb2c90387fab04534137a3db7aef99
Max Henkel <max@maxhenkel.de>
2025-08-20 22:43:12 +0200

    Fix settings if natives are disabled or not available

--- common-client/src/main/java/de/maxhenkel/voicechat/gui/widgets/VadButton.java
+++ common-client/src/main/java/de/maxhenkel/voicechat/gui/widgets/VadButton.java
@@ -16,6 +16,7 @@ public class VadButton extends BooleanConfigButton {
         });
         if (!Denoiser.canUseDenoiser()) {
             active = false;
+            setMessage(component.apply(false));
         }
     }
 
