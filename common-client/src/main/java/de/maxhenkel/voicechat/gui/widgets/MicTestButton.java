4cf8eea0eb9cae7b74031ab6fddbefc011aaf054
Max Henkel <max@maxhenkel.de>
2025-08-20 15:49:30 +0200

    Rework microphone processing

--- common-client/src/main/java/de/maxhenkel/voicechat/gui/widgets/MicTestButton.java
+++ common-client/src/main/java/de/maxhenkel/voicechat/gui/widgets/MicTestButton.java
@@ -133,7 +133,6 @@ public class MicTestButton extends ToggleImageButton implements ImageButton.Tool
 
     private class VoiceThread extends Thread {
 
-        private final MicActivator micActivator;
         private final Speaker speaker;
         private boolean running;
         private long lastRender;
@@ -147,8 +146,6 @@ public class MicTestButton extends ToggleImageButton implements ImageButton.Tool
             setDaemon(true);
             setName("VoiceTestingThread");
             setUncaughtExceptionHandler(new VoicechatUncaughtExceptionHandler());
-
-            micActivator = new MicActivator();
 
             micThread = client != null ? client.getMicThread() : null;
             if (micThread == null) {
@@ -182,23 +179,16 @@ public class MicTestButton extends ToggleImageButton implements ImageButton.Tool
                 if (System.currentTimeMillis() - lastRender > 500L) {
                     break;
                 }
-                short[] buff = micThread.pollMic();
+                short[] buff = micThread.pollProcessedAudio(true);
                 if (buff == null) {
                     continue;
                 }
 
                 micListener.onMicValue(AudioUtils.getHighestAudioLevel(buff));
 
-                if (VoicechatClient.CLIENT_CONFIG.microphoneActivationType.get().equals(MicrophoneActivationType.VOICE)) {
-                    if (micActivator.push(buff, a -> {
-                    })) {
-                        play(buff);
-                    }
-                } else {
-                    micActivator.stopActivating();
+                if (micThread.shouldTransmitAudio()) {
                     play(buff);
                 }
-
             }
             speaker.close();
             setMicLocked(false);
