7c7580367f696bfa8c919f14a0fdce54a173549f
Max Henkel <max@maxhenkel.de>
2025-08-23 21:39:00 +0200

    Keep backwards compatibility with flashback

--- common-client/src/main/java/de/maxhenkel/voicechat/voice/client/TalkCache.java
+++ common-client/src/main/java/de/maxhenkel/voicechat/voice/client/TalkCache.java
@@ -29,7 +29,7 @@ public class TalkCache {
         this.timestampSupplier = timestampSupplier;
     }
 
-    public void updateTalking(UUID entity, boolean whispering, double audioLevel) {
+    private void updateTalking(UUID entity, boolean whispering, double audioLevel) {
         PlayerCache talk = playerCache.get(entity);
         if (talk == null) {
             talk = new PlayerCache(timestampSupplier.get(), whispering, audioLevel);
@@ -42,7 +42,24 @@ public class TalkCache {
     }
 
     /**
+     * This just exists for backwards compatibility with flashback
+     *
+     * @param entity     the entity UUID
+     * @param whispering if the entity is whispering
+     * @deprecated Use {@link #updateLevel(UUID, String, boolean, short[])} instead
+     */
+    @Deprecated
+    public void updateTalking(UUID entity, boolean whispering) {
+        updateTalking(entity, whispering, AudioUtils.LOWEST_DB);
+    }
+
+    /**
      * Updates the audio level of a player talking or a specific category
+     *
+     * @param id         the entity UUID
+     * @param category   the category name or null if it is a player
+     * @param whispering if the entity is whispering
+     * @param audio      the audio data to calculate the audio level from
      */
     public void updateLevel(UUID id, @Nullable String category, boolean whispering, short[] audio) {
         double highestAudioLevel = AudioUtils.getHighestAudioLevel(audio);
@@ -51,10 +68,6 @@ public class TalkCache {
         }
         // Update the player talking even if it is a category
         updateTalking(id, whispering, highestAudioLevel);
-    }
-
-    public void updateTalking(UUID entity, boolean whispering, short[] audio) {
-        updateTalking(entity, whispering, AudioUtils.getHighestAudioLevel(audio));
     }
 
     public boolean isTalking(Entity entity) {
