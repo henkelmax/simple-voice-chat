d0d9c45812468cf727091761fd9ee3ed9730bc0a
Max Henkel <max@maxhenkel.de>
2025-08-24 19:11:47 +0200

    Add test speaker button in speaker selection list

---
+++ common-client/src/main/java/de/maxhenkel/voicechat/gui/audiodevice/SpeakerAudioDeviceEntry.java
@@ -0,0 +1,44 @@
+package de.maxhenkel.voicechat.gui.audiodevice;
+
+import de.maxhenkel.voicechat.Voicechat;
+import de.maxhenkel.voicechat.gui.tooltips.TestSpeakerSupplier;
+import de.maxhenkel.voicechat.gui.widgets.ImageButton;
+import de.maxhenkel.voicechat.voice.client.TestSoundPlayer;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Supplier;
+
+public class SpeakerAudioDeviceEntry extends AudioDeviceEntry {
+
+    public static final ResourceLocation SPEAKER_ICON = ResourceLocation.fromNamespaceAndPath(Voicechat.MODID, "textures/icons/test_speaker.png");
+
+    private ImageButton testButton;
+
+    public SpeakerAudioDeviceEntry(String device, Component name, @Nullable ResourceLocation icon, Supplier<Boolean> isSelected) {
+        super(device, name, icon, isSelected);
+
+        testButton = new ImageButton(0, 0, SPEAKER_ICON, button -> {
+            testButton.active = false;
+            TestSoundPlayer.playTestSound(() -> {
+                testButton.active = true;
+            });
+        }, new TestSpeakerSupplier());
+        children.add(testButton);
+    }
+
+    @Override
+    public void render(GuiGraphics guiGraphics, int index, int top, int left, int width, int height, int mouseX, int mouseY, boolean hovered, float delta) {
+        super.render(guiGraphics, index, top, left, width, height, mouseX, mouseY, hovered, delta);
+        boolean selected = isSelected.get();
+        if (selected && hovered) {
+            testButton.visible = true;
+            testButton.setPosition(left + (width - testButton.getWidth() - PADDING), top + (height - testButton.getHeight()) / 2);
+            testButton.render(guiGraphics, mouseX, mouseY, delta);
+        } else {
+            testButton.visible = false;
+        }
+    }
+}
