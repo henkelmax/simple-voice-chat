ea8a3da750a73bfa97613e9b5f86320972ba9687
Max Henkel <max@maxhenkel.de>
2025-08-25 13:07:15 +0200

    Fix annotations

--- common-client/src/main/java/de/maxhenkel/voicechat/gui/audiodevice/MicrophoneAudioDeviceEntry.java
+++ common-client/src/main/java/de/maxhenkel/voicechat/gui/audiodevice/MicrophoneAudioDeviceEntry.java
@@ -4,8 +4,8 @@ import de.maxhenkel.voicechat.gui.widgets.MicTestButton;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
-import org.jetbrains.annotations.Nullable;
 
+import javax.annotation.Nullable;
 import java.util.function.Supplier;
 
 public class MicrophoneAudioDeviceEntry extends AudioDeviceEntry {
