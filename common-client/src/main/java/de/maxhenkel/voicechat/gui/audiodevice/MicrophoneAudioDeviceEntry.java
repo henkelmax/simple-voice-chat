3f76c5579771c59597dc590da9b0bd79d60e5765
Max Henkel <max@maxhenkel.de>
2025-08-25 11:10:54 +0200

    Add microphone test button in microphone selection list

---
+++ common-client/src/main/java/de/maxhenkel/voicechat/gui/audiodevice/MicrophoneAudioDeviceEntry.java
@@ -0,0 +1,29 @@
+package de.maxhenkel.voicechat.gui.audiodevice;
+
+import de.maxhenkel.voicechat.gui.widgets.MicTestButton;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Supplier;
+
+public class MicrophoneAudioDeviceEntry extends AudioDeviceEntry {
+
+    private final MicTestButton testButton;
+
+    public MicrophoneAudioDeviceEntry(String device, Component name, @Nullable ResourceLocation icon, Supplier<Boolean> isSelected, MicTestButton testButton) {
+        super(device, name, icon, isSelected);
+        this.testButton = testButton;
+    }
+
+    @Override
+    public void render(GuiGraphics guiGraphics, int index, int top, int left, int width, int height, int mouseX, int mouseY, boolean hovered, float delta) {
+        super.render(guiGraphics, index, top, left, width, height, mouseX, mouseY, hovered, delta);
+        boolean selected = isSelected.get();
+        if (selected && (hovered || testButton.isMicActive())) {
+            testButton.setPosition(left + (width - testButton.getWidth() - PADDING), top + (height - testButton.getHeight()) / 2);
+            testButton.render(guiGraphics, mouseX, mouseY, delta);
+        }
+    }
+}
