9c66633309ac0c67609f76b727a21d3adba39bca
Max Henkel <max@maxhenkel.de>
2025-09-04 15:54:08 +0200

    Fix groups, player states and audio categories not being cleared when switching servers on a proxy

--- fabric/src/main/java/de/maxhenkel/voicechat/intercompatibility/FabricClientCompatibilityManager.java
+++ fabric/src/main/java/de/maxhenkel/voicechat/intercompatibility/FabricClientCompatibilityManager.java
@@ -82,6 +82,11 @@ public class FabricClientCompatibilityManager extends ClientCompatibilityManager
     }
 
     @Override
+    public void emitDisconnectedEvent() {
+        ClientWorldEvents.DISCONNECT.invoker().run();
+    }
+
+    @Override
     public void onDisconnect(Runnable onDisconnect) {
         ClientWorldEvents.DISCONNECT.register(onDisconnect);
     }
