8a6d7bbc8ca1355b6d518d37728ce569c90b2497
Max Henkel <max@maxhenkel.de>
2025-09-10 18:16:32 +0200

    Fix potential mixin conflict

--- fabric/src/main/java/de/maxhenkel/voicechat/intercompatibility/FabricClientCompatibilityManager.java
+++ fabric/src/main/java/de/maxhenkel/voicechat/intercompatibility/FabricClientCompatibilityManager.java
@@ -109,6 +109,6 @@ public class FabricClientCompatibilityManager extends ClientCompatibilityManager
     @Override
     public void addResourcePackSource(PackRepository packRepository, RepositorySource repositorySource) {
         IPackRepository repository = (IPackRepository) packRepository;
-        repository.addSource(repositorySource);
+        repository.voicechat$addSource(repositorySource);
     }
 }
