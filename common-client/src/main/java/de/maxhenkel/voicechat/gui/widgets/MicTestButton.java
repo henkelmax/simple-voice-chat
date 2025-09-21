adae57d6f9d10957c562110a462c05552dc64f87
Max Henkel <max@maxhenkel.de>
2025-09-21 20:59:50 +0200

    Clean up GUI code

--- common-client/src/main/java/de/maxhenkel/voicechat/gui/widgets/MicTestButton.java
+++ common-client/src/main/java/de/maxhenkel/voicechat/gui/widgets/MicTestButton.java
@@ -62,11 +62,6 @@ public class MicTestButton extends ToggleImageButton implements ImageButton.Tool
         }
     }
 
-    @Override
-    protected boolean shouldRenderTooltip() {
-        return false;
-    }
-
     public void setMicActive(boolean micActive) {
         this.micActive = micActive;
     }
