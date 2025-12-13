66eb7eaec72d1be838cbc9e900b091336d281264
Max Henkel <max@maxhenkel.de>
2025-12-13 12:03:28 +0100

    Fix potential audio issues and resource leaks and improve error handling

--- common-client/src/main/java/de/maxhenkel/voicechat/gui/widgets/MicTestButton.java
+++ common-client/src/main/java/de/maxhenkel/voicechat/gui/widgets/MicTestButton.java
@@ -172,7 +172,7 @@ public class MicTestButton extends ToggleImageButton implements ImageButton.Tool
 
             SoundManager soundManager;
             if (client == null) {
-                soundManager = new SoundManager();
+                soundManager = SoundManager.create();
                 ownSoundManager = soundManager;
             } else {
                 soundManager = client.getSoundManager();
