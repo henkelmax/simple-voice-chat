abe9aceabaf7559363b39ee4b3229f5a9046864b
Max Henkel <max@maxhenkel.de>
2025-12-13 12:03:28 +0100

    Fix potential audio issues and resource leaks and improve error handling

--- common-client/src/main/java/de/maxhenkel/voicechat/voice/client/SoundManager.java
+++ common-client/src/main/java/de/maxhenkel/voicechat/voice/client/SoundManager.java
@@ -24,28 +24,81 @@ public class SoundManager {
     private final ALCapabilities alCaps;
     private final float maxGain;
 
-    public SoundManager(@Nullable String deviceName) throws SpeakerException {
+    public SoundManager(@Nullable String deviceName, long device, long context, ALCCapabilities alcCaps, ALCapabilities alCaps, float maxGain) {
         this.deviceName = deviceName;
-
-        device = openSpeaker(deviceName);
-        context = ALC11.alcCreateContext(device, (IntBuffer) null);
-
-        alcCaps = ALC.createCapabilities(device);
-        alCaps = AL.createCapabilities(alcCaps);
-
-        if (alCaps.AL_SOFT_gain_clamp_ex) {
-            maxGain = AL11.alGetFloat(SOFTGainClampEx.AL_GAIN_LIMIT_SOFT);
-            checkAlcError(device);
-        } else {
-            maxGain = 1F;
-            Voicechat.LOGGER.warn("OpenAL extension 'AL_SOFT_gain_clamp_ex' not supported - Voice chat volume can't exceed 100%");
-        }
-
-        ClientPluginManager.instance().onCreateALContext(context, device);
+        this.device = device;
+        this.context = context;
+        this.alcCaps = alcCaps;
+        this.alCaps = alCaps;
+        this.maxGain = maxGain;
     }
 
-    public SoundManager() throws SpeakerException {
-        this(VoicechatClient.CLIENT_CONFIG.speaker.get());
+    public static SoundManager create() throws SpeakerException {
+        return create(VoicechatClient.CLIENT_CONFIG.speaker.get());
+    }
+
+    public static SoundManager create(@Nullable String deviceName) throws SpeakerException {
+        long prevContext = ALC11.alcGetCurrentContext();
+        long prevDevice = (prevContext != 0L) ? ALC11.alcGetContextsDevice(prevContext) : 0L;
+        try {
+            long device = openSpeaker(deviceName);
+            long context = ALC11.alcCreateContext(device, (IntBuffer) null);
+            if (context == 0L) {
+                int error = ALC11.alcGetError(device);
+                ALC11.alcCloseDevice(device);
+                checkAlcError(device);
+                throw new SpeakerException("Failed to create OpenAL context: %s".formatted(getAlcError(error)));
+            }
+            if (!ALC11.alcMakeContextCurrent(context)) {
+                int error = ALC11.alcGetError(device);
+                ALC11.alcDestroyContext(context);
+                checkAlcError(device);
+                ALC11.alcCloseDevice(device);
+                checkAlcError(device);
+                throw new SpeakerException("Failed to make OpenAL context current: %s".formatted(getAlcError(error)));
+            }
+
+            ALCCapabilities alcCaps = ALC.createCapabilities(device);
+            ALCapabilities alCaps = AL.createCapabilities(alcCaps);
+
+            float maxGain;
+
+            if (alCaps.AL_SOFT_gain_clamp_ex) {
+                maxGain = AL11.alGetFloat(SOFTGainClampEx.AL_GAIN_LIMIT_SOFT);
+                checkAlcError(device);
+            } else {
+                maxGain = 1F;
+                Voicechat.LOGGER.warn("OpenAL extension 'AL_SOFT_gain_clamp_ex' not supported - Voice chat volume can't exceed 100%");
+            }
+
+            ClientPluginManager.instance().onCreateALContext(context, device);
+
+            return new SoundManager(deviceName, device, context, alcCaps, alCaps, maxGain);
+        } catch (SpeakerException speakerException) {
+            throw speakerException;
+        } catch (Throwable t) {
+            throw new SpeakerException("Failed to initialize OpenAL context", t);
+        } finally {
+            try {
+                if (prevContext != 0L) {
+                    if (!ALC11.alcMakeContextCurrent(prevContext)) {
+                        if (prevDevice != 0L) {
+                            int error = ALC11.alcGetError(prevDevice);
+                            Voicechat.LOGGER.error("Failed to restore previous OpenAL context ({}): {}", prevContext, getAlcError(error));
+                        } else {
+                            Voicechat.LOGGER.error("Failed to restore previous OpenAL context ({}): Device not found", prevContext);
+                        }
+                    } else {
+                        if (prevDevice != 0L) {
+                            ALCCapabilities prevAlcCaps = ALC.createCapabilities(prevDevice);
+                            AL.createCapabilities(prevAlcCaps);
+                        }
+                    }
+                }
+            } catch (Throwable t) {
+                Voicechat.LOGGER.warn("Failed to restore previous OpenAL context", t);
+            }
+        }
     }
 
     public void close() {
@@ -70,7 +123,7 @@ public class SoundManager {
         return context == 0 || device == 0;
     }
 
-    private long openSpeaker(@Nullable String name) throws SpeakerException {
+    private static long openSpeaker(@Nullable String name) throws SpeakerException {
         try {
             return tryOpenSpeaker(name);
         } catch (SpeakerException e) {
@@ -85,7 +138,7 @@ public class SoundManager {
         }
     }
 
-    private long tryOpenSpeaker(@Nullable String string) throws SpeakerException {
+    private static long tryOpenSpeaker(@Nullable String string) throws SpeakerException {
         long l = ALC11.alcOpenDevice(string);
         if (l == 0L) {
             throw new SpeakerException("Failed to open audio device: Audio device not found");
@@ -167,8 +220,8 @@ public class SoundManager {
         return true;
     }
 
-    private static String getAlError(int i) {
-        switch (i) {
+    private static String getAlError(int errorCode) {
+        switch (errorCode) {
             case AL11.AL_INVALID_NAME:
                 return "Invalid name";
             case AL11.AL_INVALID_ENUM:
@@ -180,7 +233,7 @@ public class SoundManager {
             case AL11.AL_OUT_OF_MEMORY:
                 return "Out of memory";
             default:
-                return "Unknown error";
+                return "Error %#X".formatted(errorCode);
         }
     }
 
