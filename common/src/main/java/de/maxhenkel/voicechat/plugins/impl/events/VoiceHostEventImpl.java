8629cf42a4dce16157c5f64b4242e491efd242ec
Max Henkel <max@maxhenkel.de>
2026-02-28 12:59:59 +0100

    Backport changes

--- common/src/main/java/de/maxhenkel/voicechat/plugins/impl/events/VoiceHostEventImpl.java
+++ common/src/main/java/de/maxhenkel/voicechat/plugins/impl/events/VoiceHostEventImpl.java
@@ -2,14 +2,14 @@ package de.maxhenkel.voicechat.plugins.impl.events;
 
 import de.maxhenkel.voicechat.api.events.VoiceHostEvent;
 import de.maxhenkel.voicechat.plugins.impl.ServerPlayerImpl;
-import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.entity.player.EntityPlayerMP;
 
 public class VoiceHostEventImpl extends ServerEventImpl implements VoiceHostEvent {
 
     private final ServerPlayerImpl player;
     private String voiceHost;
 
-    public VoiceHostEventImpl(ServerPlayerEntity player, String voiceHost) {
+    public VoiceHostEventImpl(EntityPlayerMP player, String voiceHost) {
         this.player = new ServerPlayerImpl(player);
         this.voiceHost = voiceHost;
     }
