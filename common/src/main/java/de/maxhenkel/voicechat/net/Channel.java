49185c82a3b01f2804dffd5c1e3ed4d0860e1537
Max Henkel <max@maxhenkel.de>
2026-03-24 15:14:28 +0100

    Backport changes

--- common/src/main/java/de/maxhenkel/voicechat/net/Channel.java
+++ common/src/main/java/de/maxhenkel/voicechat/net/Channel.java
@@ -2,8 +2,8 @@ package de.maxhenkel.voicechat.net;
 
 import de.maxhenkel.voicechat.Voicechat;
 import de.maxhenkel.voicechat.intercompatibility.CommonCompatibilityManager;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
 
@@ -25,7 +25,7 @@ public class Channel<T extends Packet<T>> {
     public void onServerPacket(MinecraftServer server, ServerPlayer player, ServerGamePacketListenerImpl handler, T packet) {
         if (!Voicechat.SERVER.getRateLimiter().allow(player.getUUID())) {
             Voicechat.LOGGER.warn("Player {} exceeded packet rate limit", player.getName().getString());
-            player.connection.disconnect(Component.translatableWithFallback("disconnect.exceeded_packet_rate", "Kicked for exceeding packet rate limit"));
+            player.connection.disconnect(new TranslatableComponent("disconnect.exceeded_packet_rate"));
             return;
         }
         CommonCompatibilityManager.INSTANCE.execute(server, () -> {
