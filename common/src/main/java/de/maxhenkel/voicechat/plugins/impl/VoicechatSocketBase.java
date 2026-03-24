66163d7c0dfb09dd081a48e0400d97d6806b8fab
Max Henkel <max@maxhenkel.de>
2026-03-24 14:14:17 +0100

    Prevent processing of UDP packets that are too large

--- common/src/main/java/de/maxhenkel/voicechat/plugins/impl/VoicechatSocketBase.java
+++ common/src/main/java/de/maxhenkel/voicechat/plugins/impl/VoicechatSocketBase.java
@@ -1,5 +1,8 @@
 package de.maxhenkel.voicechat.plugins.impl;
 
+import de.maxhenkel.voicechat.Voicechat;
+import de.maxhenkel.voicechat.debug.CooldownTimer;
+
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -11,6 +14,12 @@ public class VoicechatSocketBase {
     public RawUdpPacketImpl read(DatagramSocket socket) throws IOException {
         DatagramPacket packet = new DatagramPacket(BUFFER, BUFFER.length);
         socket.receive(packet);
+        if (packet.getLength() >= BUFFER.length) {
+            CooldownTimer.run("udp_packet_too_large", () -> {
+                Voicechat.LOGGER.warn("Packet from {} is too large", packet.getSocketAddress());
+            });
+            throw new IOException(String.format("Packet from %s is too large", packet.getSocketAddress()));
+        }
         // Setting the timestamp after receiving the packet
         long timestamp = System.currentTimeMillis();
         byte[] data = new byte[packet.getLength()];
