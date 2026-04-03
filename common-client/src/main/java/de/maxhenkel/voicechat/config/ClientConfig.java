352948fca1046b6b08f52f4057e6ad46a03317a4
Max Henkel <max@maxhenkel.de>
2026-04-03 17:09:45 +0200

    Fall back to Java microphone implementation when using ALSoft >= 1.25.0

--- common-client/src/main/java/de/maxhenkel/voicechat/config/ClientConfig.java
+++ common-client/src/main/java/de/maxhenkel/voicechat/config/ClientConfig.java
@@ -208,8 +208,9 @@ public class ClientConfig {
                         "If the voice chat should work in singleplayer or in worlds shared over LAN"
                 );
         javaMicrophoneImplementation = builder
-                .booleanEntry("java_microphone_implementation", Platform.isMac(),
-                        "Whether to use the Java implementation of microphone capture instead of OpenAL"
+                .booleanEntry("java_microphone_implementation", false,
+                        "Whether to use the Java implementation of microphone capture instead of OpenAL",
+                        "Note that having this set to false doesn't necessarily mean the mod will use OpenAL - Some operating systems or Minecraft versions might not support it properly"
                 );
         showFakePlayersDisconnected = builder
                 .booleanEntry("show_fake_players_disconnected", false,
