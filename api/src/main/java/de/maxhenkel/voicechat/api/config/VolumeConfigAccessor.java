d13e8f1ead84a01bf9eb2edc3d8bae038a8fca3d
Max Henkel <max@maxhenkel.de>
2026-06-18 21:28:14 +0200

    Update javadocs for `VolumeConfigAccessor`

--- api/src/main/java/de/maxhenkel/voicechat/api/config/VolumeConfigAccessor.java
+++ api/src/main/java/de/maxhenkel/voicechat/api/config/VolumeConfigAccessor.java
@@ -12,11 +12,18 @@ import java.util.UUID;
 public interface VolumeConfigAccessor {
 
     /**
+     * Gets the volume of a player by its UUID.
+     * <br/>
+     * <br/>
      * This will always return a value, even if the player is not in the volume config.
+     * <br/>
      * By default, the volume will be <code>1.0</code>.
      * A volume of <code>0.0</code> means this player is muted.
      * Can be larger than <code>1.0</code>.
      * Also works for players that are not online.
+     * <br/>
+     * <br/>
+     * If you want the <code>Other</code> volume (All volumes that are not covered by categories or players), use the UUID <code>00000000-0000-0000-0000-000000000000</code>.
      *
      * @param uuid the UUID of the player
      * @return the volume of the player
@@ -25,7 +32,11 @@ public interface VolumeConfigAccessor {
     double getVolume(UUID uuid);
 
     /**
+     * Gets the volume of a category by its UUID.
+     * <br/>
+     * <br/>
      * This will always return a value, even if the category doesn't exist.
+     * <br/>
      * By default, the volume will be <code>1.0</code>.
      * A volume of <code>0.0</code> means this category is muted.
      * Can be larger than <code>1.0</code>.
