4fba592ac37ec0d4737224eb01c0428b51177d1a
Max Henkel <max@maxhenkel.de>
2026-07-18 19:57:26 +0200

    Improve Folia support

--- common/src/main/java/de/maxhenkel/voicechat/voice/server/Server.java
+++ common/src/main/java/de/maxhenkel/voicechat/voice/server/Server.java
@@ -443,7 +443,7 @@ public class Server extends Thread {
             source = SoundPacketEvent.SOURCE_PROXIMITY;
         }
 
-        broadcast(ServerWorldUtils.getPlayersInRange(sender.serverLevel(), sender.position(), getBroadcastRange(distance), p -> !p.getUUID().equals(sender.getUUID())), soundPacket, sender, senderState, groupId, source);
+        broadcast(ServerPlayerManager.getPlayersInRange(sender.serverLevel(), sender.position(), getBroadcastRange(distance), p -> !p.getUUID().equals(sender.getUUID())), soundPacket, sender, senderState, groupId, source);
     }
 
     public void sendSoundPacket(@Nullable ServerPlayer sender, @Nullable PlayerState senderState, ServerPlayer receiver, PlayerState receiverState, @Nullable ClientConnection connection, SoundPacket<?> soundPacket, String source) {
