12b7bf6b7ddafcb0df05037816f1bda3dc99e6df
Max Henkel <max@maxhenkel.de>
2026-07-19 16:08:49 +0200

    Update quilt to 1.21.4

--- quilt/src/main/java/de/maxhenkel/voicechat/QuiltVoicechatMod.java
+++ quilt/src/main/java/de/maxhenkel/voicechat/QuiltVoicechatMod.java
@@ -1,13 +1,13 @@
 package de.maxhenkel.voicechat;
 
 import de.maxhenkel.voicechat.integration.ViaVersionCompatibility;
+import net.fabricmc.api.ModInitializer;
 import org.quiltmc.loader.api.ModContainer;
-import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 
 public class QuiltVoicechatMod extends Voicechat implements ModInitializer {
 
     @Override
-    public void onInitialize(ModContainer mod) {
+    public void onInitialize() {
         initialize();
         ViaVersionCompatibility.register();
     }
--- quilt/src/main/java/de/maxhenkel/voicechat/events/InputEvents.java
+++ quilt/src/main/java/de/maxhenkel/voicechat/events/InputEvents.java
@@ -1,23 +1,24 @@
 package de.maxhenkel.voicechat.events;
 
 import de.maxhenkel.voicechat.intercompatibility.ClientCompatibilityManager;
-import org.quiltmc.qsl.base.api.event.Event;
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
 
 public class InputEvents {
 
-    public static final Event<ClientCompatibilityManager.KeyboardEvent> KEYBOARD_KEY = Event.create(ClientCompatibilityManager.KeyboardEvent.class, (listeners) -> (window, key, scancode) -> {
+    public static final Event<ClientCompatibilityManager.KeyboardEvent> KEYBOARD_KEY = EventFactory.createArrayBacked(ClientCompatibilityManager.KeyboardEvent.class, (listeners) -> (window, key, scancode) -> {
         for (ClientCompatibilityManager.KeyboardEvent event : listeners) {
             event.onKeyboardEvent(window, key, scancode);
         }
     });
 
-    public static final Event<ClientCompatibilityManager.MouseEvent> MOUSE_KEY = Event.create(ClientCompatibilityManager.MouseEvent.class, (listeners) -> (window, button, action, mods) -> {
+    public static final Event<ClientCompatibilityManager.MouseEvent> MOUSE_KEY = EventFactory.createArrayBacked(ClientCompatibilityManager.MouseEvent.class, (listeners) -> (window, button, action, mods) -> {
         for (ClientCompatibilityManager.MouseEvent event : listeners) {
             event.onMouseEvent(window, button, action, mods);
         }
     });
 
-    public static final Event<Runnable> HANDLE_KEYBINDS = Event.create(Runnable.class, (listeners) -> () -> {
+    public static final Event<Runnable> HANDLE_KEYBINDS = EventFactory.createArrayBacked(Runnable.class, (listeners) -> () -> {
         for (Runnable event : listeners) {
             event.run();
         }
