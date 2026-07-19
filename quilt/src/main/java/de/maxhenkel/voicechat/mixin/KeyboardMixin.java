3be473be55bf9dab390d0c328ae6076c85e9a0f4
Max Henkel <max@maxhenkel.de>
2026-07-19 16:55:37 +0200

    Update quilt to 1.21.10

--- quilt/src/main/java/de/maxhenkel/voicechat/mixin/KeyboardMixin.java
+++ quilt/src/main/java/de/maxhenkel/voicechat/mixin/KeyboardMixin.java
@@ -2,6 +2,7 @@ package de.maxhenkel.voicechat.mixin;
 
 import de.maxhenkel.voicechat.events.InputEvents;
 import net.minecraft.client.KeyboardHandler;
+import net.minecraft.client.input.KeyEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,9 +11,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(KeyboardHandler.class)
 public class KeyboardMixin {
 
-    @Inject(at = @At("HEAD"), method = "keyPress")
-    private void onKey(long window, int key, int scancode, int i, int j, CallbackInfo info) {
-        InputEvents.KEYBOARD_KEY.invoker().onKeyboardEvent(window, key, scancode);
+    @Inject(method = "keyPress", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;getWindow()Lcom/mojang/blaze3d/platform/Window;", shift =  At.Shift.AFTER))
+    private void onKey(long windowHandle, int key, KeyEvent keyEvent, CallbackInfo ci) {
+        InputEvents.KEYBOARD_KEY.invoker().onKeyboardEvent(keyEvent);
     }
 
 }
