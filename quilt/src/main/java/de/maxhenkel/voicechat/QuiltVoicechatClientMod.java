e2e9c6da0d9672b7d5ab0b7a74858f72ab9a1332
Max Henkel <max@maxhenkel.de>
2026-07-19 16:08:49 +0200

    Update quilt to 1.21.4

--- quilt/src/main/java/de/maxhenkel/voicechat/QuiltVoicechatClientMod.java
+++ quilt/src/main/java/de/maxhenkel/voicechat/QuiltVoicechatClientMod.java
@@ -1,13 +1,12 @@
 package de.maxhenkel.voicechat;
 
-import org.quiltmc.loader.api.ModContainer;
-import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 import de.maxhenkel.voicechat.integration.clothconfig.ClothConfig;
 
 public class QuiltVoicechatClientMod extends VoicechatClient implements ClientModInitializer {
 
     @Override
-    public void onInitializeClient(ModContainer mod) {
+    public void onInitializeClient() {
         initializeClient();
         ClothConfig.init();
     }
--- quilt/src/main/java/de/maxhenkel/voicechat/events/ClientVoiceChatEvents.java
+++ quilt/src/main/java/de/maxhenkel/voicechat/events/ClientVoiceChatEvents.java
@@ -1,19 +1,20 @@
 package de.maxhenkel.voicechat.events;
 
 import de.maxhenkel.voicechat.voice.client.ClientVoicechatConnection;
-import org.quiltmc.qsl.base.api.event.Event;
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
 
 import java.util.function.Consumer;
 
 public class ClientVoiceChatEvents {
 
-    public static final Event<Consumer<ClientVoicechatConnection>> VOICECHAT_CONNECTED = Event.create(Consumer.class, (listeners) -> (client) -> {
+    public static final Event<Consumer<ClientVoicechatConnection>> VOICECHAT_CONNECTED = EventFactory.createArrayBacked(Consumer.class, (listeners) -> (client) -> {
         for (Consumer<ClientVoicechatConnection> listener : listeners) {
             listener.accept(client);
         }
     });
 
-    public static final Event<Runnable> VOICECHAT_DISCONNECTED = Event.create(Runnable.class, (listeners) -> () -> {
+    public static final Event<Runnable> VOICECHAT_DISCONNECTED = EventFactory.createArrayBacked(Runnable.class, (listeners) -> () -> {
         for (Runnable listener : listeners) {
             listener.run();
         }
