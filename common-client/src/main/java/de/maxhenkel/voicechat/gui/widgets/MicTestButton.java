7a40b779d48190c2b818ae7f86db962d8a67af45
Max Henkel <max@maxhenkel.de>
2026-07-20 16:24:22 +0200

    Properly track speaker creation

--- common-client/src/main/java/de/maxhenkel/voicechat/gui/widgets/MicTestButton.java
+++ common-client/src/main/java/de/maxhenkel/voicechat/gui/widgets/MicTestButton.java
@@ -161,7 +161,6 @@ public class MicTestButton extends ToggleImageButton implements ImageButton.Tool
                     soundManager = SoundManager.create();
                 }
                 speaker = SpeakerManager.createSpeaker(soundManager, null);
-                soundManager.trackSpeaker(speaker);
 
                 while (running) {
                     if (System.currentTimeMillis() - lastRender > 500L) {
@@ -188,7 +187,6 @@ public class MicTestButton extends ToggleImageButton implements ImageButton.Tool
             } finally {
                 if (speaker != null) {
                     speaker.close();
-                    soundManager.untrackSpeaker(speaker);
                 }
                 if (ownSoundManager && soundManager != null) {
                     soundManager.close();
