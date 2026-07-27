bd08a7a868f8e773a9e2ac0f3da6f5a012478b0d
Max Henkel <max@maxhenkel.de>
2026-07-27 13:11:10 +0200

    Prevent audio channels causing infinite hanging

--- common-client/src/main/java/de/maxhenkel/voicechat/voice/client/AudioChannel.java
+++ common-client/src/main/java/de/maxhenkel/voicechat/voice/client/AudioChannel.java
@@ -37,7 +37,7 @@ public class AudioChannel extends Thread {
     private final AudioPacketBuffer packetBuffer;
     private long lastPacketTime;
     private Speaker speaker;
-    private boolean stopped;
+    private volatile boolean stopped;
     private final OpusDecoder decoder;
     private long lastSequenceNumber;
     private long lostPackets;
@@ -71,9 +71,11 @@ public class AudioChannel extends Thread {
         if (Thread.currentThread() == this) {
             return;
         }
-        interrupt();
         try {
-            join();
+            join(3_000L);
+            if (isAlive()) {
+                Voicechat.LOGGER.warn("Timed out waiting for audio channel {} to close", uuid);
+            }
         } catch (InterruptedException e) {
             Voicechat.LOGGER.error("Interrupted while waiting for audio channel to close", e);
         }
