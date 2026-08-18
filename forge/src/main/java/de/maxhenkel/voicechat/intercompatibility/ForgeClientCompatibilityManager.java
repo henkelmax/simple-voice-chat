91cf512e1a55066b9fb1a0ad7189fa375b20f8c7
Max Henkel <max@maxhenkel.de>
2026-08-18 10:02:15 +0200

    Improved resiliency against mods that change the camera off-thread (#929)

--- forge/src/main/java/de/maxhenkel/voicechat/intercompatibility/ForgeClientCompatibilityManager.java
+++ forge/src/main/java/de/maxhenkel/voicechat/intercompatibility/ForgeClientCompatibilityManager.java
@@ -36,6 +36,7 @@ public class ForgeClientCompatibilityManager extends ClientCompatibilityManager 
     private final List<KeyboardEvent> keyboardEvents;
     private final List<MouseEvent> mouseEvents;
     private final List<Runnable> clientTickEvents;
+    private final List<Runnable> renderTickEvents;
     private final List<Runnable> inputEvents;
     private final List<Runnable> disconnectEvents;
     private final List<Runnable> joinWorldEvents;
@@ -51,6 +52,7 @@ public class ForgeClientCompatibilityManager extends ClientCompatibilityManager 
         keyboardEvents = new CopyOnWriteArrayList<>();
         mouseEvents = new CopyOnWriteArrayList<>();
         clientTickEvents = new CopyOnWriteArrayList<>();
+        renderTickEvents = new CopyOnWriteArrayList<>();
         inputEvents = new CopyOnWriteArrayList<>();
         disconnectEvents = new CopyOnWriteArrayList<>();
         joinWorldEvents = new CopyOnWriteArrayList<>();
@@ -95,6 +97,11 @@ public class ForgeClientCompatibilityManager extends ClientCompatibilityManager 
     @SubscribeEvent
     public void onInput(TickEvent.ClientTickEvent event) {
         inputEvents.forEach(Runnable::run);
+    }
+
+    @SubscribeEvent
+    public void onRenderTick(TickEvent.RenderTickEvent.Pre event) {
+        renderTickEvents.forEach(Runnable::run);
     }
 
     @SubscribeEvent
@@ -163,6 +170,11 @@ public class ForgeClientCompatibilityManager extends ClientCompatibilityManager 
     @Override
     public void onClientTick(Runnable onClientTick) {
         clientTickEvents.add(onClientTick);
+    }
+
+    @Override
+    public void onRenderTick(Runnable onRenderTick) {
+        renderTickEvents.add(onRenderTick);
     }
 
     @Override
