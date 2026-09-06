67acf1075f55cb64927d1835c9a593e46acc5f75
Max Henkel <max@maxhenkel.de>
2026-09-06 16:42:18 +0200

    Ignore `voice_host` in singleplayer and LAN worlds

--- common/src/main/java/de/maxhenkel/voicechat/net/SecretPacket.java
+++ common/src/main/java/de/maxhenkel/voicechat/net/SecretPacket.java
@@ -2,7 +2,6 @@ package de.maxhenkel.voicechat.net;
 
 import de.maxhenkel.voicechat.Voicechat;
 import de.maxhenkel.voicechat.config.ServerConfig;
-import de.maxhenkel.voicechat.plugins.PluginManager;
 import de.maxhenkel.voicechat.voice.common.Secret;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.network.PacketBuffer;
@@ -29,7 +28,7 @@ public class SecretPacket implements Packet<SecretPacket> {
 
     }
 
-    public SecretPacket(ServerPlayerEntity player, Secret secret, int port, ServerConfig serverConfig) {
+    public SecretPacket(ServerPlayerEntity player, Secret secret, int port, ServerConfig serverConfig, String voiceHost) {
         this.secret = secret;
         this.serverPort = port;
         this.playerUUID = player.getUUID();
@@ -38,7 +37,7 @@ public class SecretPacket implements Packet<SecretPacket> {
         this.voiceChatDistance = serverConfig.voiceChatDistance.get();
         this.keepAlive = serverConfig.keepAlive.get();
         this.groupsEnabled = serverConfig.groupsEnabled.get();
-        this.voiceHost = PluginManager.instance().getVoiceHost(player, serverConfig.voiceHost.get());
+        this.voiceHost = voiceHost;
         this.allowRecording = serverConfig.allowRecording.get();
     }
 
