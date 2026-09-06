ac08983402d61cd97a8e6dbec734f109b1f5e43a
Max Henkel <max@maxhenkel.de>
2026-09-06 16:07:39 +0200

    Reuse dedicated server field in voice chat server for offline mode check

--- common/src/main/java/de/maxhenkel/voicechat/voice/server/ServerVoiceEvents.java
+++ common/src/main/java/de/maxhenkel/voicechat/voice/server/ServerVoiceEvents.java
@@ -84,12 +84,6 @@ public class ServerVoiceEvents {
             return;
         }
 
-        if (mcServer instanceof DedicatedServer) {
-            if (!mcServer.usesAuthentication()) {
-                Voicechat.LOGGER.warn("Running in offline mode - Voice chat encryption is not secure!");
-            }
-        }
-
         try {
             server = new Server(mcServer);
             server.start();
