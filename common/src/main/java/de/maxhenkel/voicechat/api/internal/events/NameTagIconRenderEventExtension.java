b82c8c14e4dea5527a5c5268b8dfbefa64fe29ce
Max Henkel <max@maxhenkel.de>
2026-09-14 18:38:19 +0200

    Implement an internal API for use by Flashback

---
+++ common/src/main/java/de/maxhenkel/voicechat/api/internal/events/NameTagIconRenderEventExtension.java
@@ -0,0 +1,10 @@
+package de.maxhenkel.voicechat.api.internal.events;
+
+/**
+ * <b>For internal use only! Do not use this!</b>
+ */
+public interface NameTagIconRenderEventExtension {
+
+    boolean isDisconnected();
+
+}
